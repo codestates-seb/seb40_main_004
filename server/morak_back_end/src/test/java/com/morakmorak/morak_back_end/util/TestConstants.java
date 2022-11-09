9b0603b1d66cfd52a356fda5a7cb0a94fdf3c0ab
yangddoddi <pud1251@gmail.com>
2022-11-09 13:11:40 +0900

    test : 단위테스트용 시큐리티 설정과 테스트 전용 상수들 작성

---
+++ server/morak_back_end/src/test/java/com/morakmorak/morak_back_end/util/TestConstants.java
@@ -0,0 +1,24 @@
+package com.morakmorak.morak_back_end.util;
+
+public class TestConstants {
+    public final static String EMAIL1 = "user@test.com";
+    public final static String EMAIL2 = "user2@test.com";
+
+    public final static String PASSWORD1 = "passWORD123!";
+    public final static String PASSWORD2 = "passWORD321!";
+    public final static Long ID1 = 1L;
+    public final static Long ID2 = 2L;
+    public final static String NICKNAME1 = "testUser1";
+    public final static String NICKNAME2 = "testUser2";
+
+    public final static String NAME1 = "김민수";
+    public final static String NAME2 = "임지민";
+
+    public final static String INVALID_EMAIL = "user@testtest";
+    public final static String INVALID_PASSWORD = "PASSWORD123";
+    public final static String INVALID_NICKNAME = "ㅋㅋㅋㅋ";
+
+    public final static String ROLE_USER = "ROLE_USER";
+    public final static String ROLE_MANAGER = "ROLE_MANAGER";
+    public final static String ROLE_ADMIN = "ROLE_ADMIN";
+}
