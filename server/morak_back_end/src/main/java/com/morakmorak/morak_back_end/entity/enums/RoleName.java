ba129a8e78d48459ba0afb1da3d71a6bd127eae3
yangddoddi <pud1251@gmail.com>
2022-11-09 14:52:35 +0900

    update : RoleName 내부 필드 및 생성자 추가

--- server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/entity/enums/RoleName.java
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/entity/enums/RoleName.java
@@ -1,4 +1,13 @@
 package com.morakmorak.morak_back_end.entity.enums;
 
 public enum RoleName {
+    ROLE_USER("ROLE_USER"),
+    ROLE_MANAGER("ROLE_MANAGER"),
+    ROLE_ADMIN("ROLE_ADMIN");
+
+    private final String name;
+
+    RoleName(String name) {
+        this.name = name;
+    }
 }
