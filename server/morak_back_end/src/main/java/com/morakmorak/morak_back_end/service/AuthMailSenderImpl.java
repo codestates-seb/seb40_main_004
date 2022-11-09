f1b1ba6930b23149fa4ee45bb26037fd132f688d
yangddoddi <pud1251@gmail.com>
2022-11-09 18:26:14 +0900

    feat : 회원가입/로그인/토큰갱신/로그아웃 서비스 계층 구현

---
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/service/AuthMailSenderImpl.java
@@ -0,0 +1,28 @@
+package com.morakmorak.morak_back_end.service;
+
+import com.morakmorak.morak_back_end.dto.EmailDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthMailSenderImpl implements AuthMailSender {
+    private final JavaMailSender mailSender;
+    private final MailProperties mailProperties;
+
+    private final String BASIC_SUBJECT = "모락모락의 이메일 인증 번호입니다.";
+    @Override
+    public boolean sendEmail(EmailDto.RequestSendMail emailDto, String authKey) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(mailProperties.getUsername());
+        simpleMailMessage.setTo(emailDto.getEmail());
+        simpleMailMessage.setSubject(BASIC_SUBJECT);
+        simpleMailMessage.setText(authKey);
+        mailSender.send(simpleMailMessage);
+
+        return true;
+    }
+}
