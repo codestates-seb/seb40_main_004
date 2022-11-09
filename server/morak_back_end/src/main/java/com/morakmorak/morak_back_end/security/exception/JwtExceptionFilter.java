72eb9146e9995ceb51483f9e4fcbac179c22514b
yangddoddi <pud1251@gmail.com>
2022-11-09 12:56:12 +0900

    feat : Jwt Exception 처리를 위한 JwtExceptionFilter 구현

---
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/security/exception/JwtExceptionFilter.java
@@ -0,0 +1,30 @@
+package com.morakmorak.morak_back_end.security.exception;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtExceptionFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (InvalidJwtTokenException e) {
+            setStatus(response);
+            response.getWriter().write(e.getErrorCode().getMessage());
+        }
+    }
+
+    private void setStatus(HttpServletResponse response) {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+    }
+}
