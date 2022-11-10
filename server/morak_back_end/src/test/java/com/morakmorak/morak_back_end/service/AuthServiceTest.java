6ed09798a617509d9d2c870b88e088c17921c010
yangddoddi <pud1251@gmail.com>
2022-11-10 19:29:45 +0900

    test : 패스워드 변경 서비스계층 단위 테스트

--- server/morak_back_end/src/test/java/com/morakmorak/morak_back_end/service/AuthServiceTest.java
+++ server/morak_back_end/src/test/java/com/morakmorak/morak_back_end/service/AuthServiceTest.java
@@ -352,4 +352,40 @@ public class AuthServiceTest {
         //then
         assertThat(result).isTrue();
     }
+
+    @Test
+    @DisplayName("패스워드 변경 요청 시, 해당 유저 DB 패스워드와 입력받은 originalPassword가 일치하지 않는다면 BusinessLogicException 발생")
+    public void changePassword_failed() {
+        //given
+        User dbUser = User.builder()
+                .id(ID1)
+                .password(PASSWORD1)
+                .build();
+
+        given(userRepository.findById(ID1)).willReturn(Optional.of(dbUser));
+        given(userPasswordManager.compareUserPassword(any(User.class), any(User.class))).willReturn(Boolean.FALSE);
+
+        //when then
+        assertThatThrownBy(() -> authService.changePassword(INVALID_PASSWORD, PASSWORD2, ID1))
+                .isInstanceOf(BusinessLogicException.class);
+    }
+
+    @Test
+    @DisplayName("패스워드 변경 요청 시, 해당 유저 DB 패스워드와 입력받은 originalPassword가 일치한다면 유저의 패스워드를 newPassword로 변경")
+    public void changePassword_success() {
+        //given
+        User dbUser = User.builder()
+                .id(ID1)
+                .password(PASSWORD1)
+                .build();
+
+        given(userRepository.findById(ID1)).willReturn(Optional.of(dbUser));
+        given(userPasswordManager.compareUserPassword(any(User.class), any(User.class))).willReturn(Boolean.TRUE);
+
+        //when
+        Boolean result = authService.changePassword(PASSWORD1, PASSWORD2, ID1);
+
+        //then
+        assertThat(result).isTrue();
+    }
 }
