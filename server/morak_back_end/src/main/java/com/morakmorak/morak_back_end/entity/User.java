23ce91b520e1e82836b40a78722ec503052fee7e
yangddoddi <pud1251@gmail.com>
2022-11-11 03:35:06 +0900

    update : User 엔티티에 코드 가독성 향상을 위한 생성자 추가

--- server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/entity/User.java
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/entity/User.java
@@ -120,6 +120,10 @@ public class User {
     @Builder.Default
     private List<Review> reviews = new ArrayList<>();
 
+    public User(String password) {
+        this.password = password;
+    }
+
     public String encryptPassword(PasswordEncoder passwordEncoder) {
         this.password = passwordEncoder.encode(this.password);
         return this.password;
