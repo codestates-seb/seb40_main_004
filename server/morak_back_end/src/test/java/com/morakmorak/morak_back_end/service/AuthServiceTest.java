4360e6df968b2b6e6b37698a892739aef47c7afe
yangddoddi <pud1251@gmail.com>
2022-11-11 02:01:14 +0900

    test : 패스워드 찾기 로직 단위 테스트

--- server/morak_back_end/src/test/java/com/morakmorak/morak_back_end/service/AuthServiceTest.java
+++ server/morak_back_end/src/test/java/com/morakmorak/morak_back_end/service/AuthServiceTest.java
@@ -5,7 +5,6 @@ import com.morakmorak.morak_back_end.adapter.UserPasswordManager;
 import com.morakmorak.morak_back_end.entity.RefreshToken;
 import com.morakmorak.morak_back_end.entity.User;
 import com.morakmorak.morak_back_end.exception.BusinessLogicException;
-import com.morakmorak.morak_back_end.exception.ErrorCode;
 import com.morakmorak.morak_back_end.repository.RedisRepository;
 import com.morakmorak.morak_back_end.repository.RoleRepository;
 import com.morakmorak.morak_back_end.repository.UserRepository;
@@ -27,6 +26,7 @@ import static com.morakmorak.morak_back_end.util.TestConstants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -59,7 +59,7 @@ public class AuthServiceTest {
     RedisRepository<String> authKeyRedisRepository;
 
     @Mock
-    AuthMailSenderImpl authMailSenderImpl;
+    MailSenderImpl authMailSenderImpl;
 
     @Test
     @DisplayName("유저 로그인 / 조회한 유저의 패스워드를 복호화하여 인자와 비교했을 때 " +
@@ -384,6 +384,57 @@ public class AuthServiceTest {
 
         //when
         Boolean result = authService.changePassword(PASSWORD1, PASSWORD2, ID1);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("패스워드 찾기 요청 시, 요청받은 이메일을 가진 유저가 존재하지 않는다면 BusinessLogicException이 발생한다.")
+    public void sendUserPasswordMail_failed() {
+        //given
+        given(userRepository.findUserByEmail(EMAIL1)).willReturn(Optional.empty());
+
+        //when then
+        assertThatThrownBy(() -> authService.sendUserPasswordEmail(EMAIL1))
+                .isInstanceOf(BusinessLogicException.class);
+    }
+
+    @Test
+    @DisplayName("패스워드 찾기 요청 시, 요청받은 이메일을 가진 유저가 존재한다면 비밀번호를 변경한다.")
+    public void sendUserPasswordMail_success() {
+        //given
+        User dbUser = User.builder()
+                .email(EMAIL1)
+                .password(PASSWORD1)
+                .build();
+
+        given(userRepository.findUserByEmail(EMAIL1)).willReturn(Optional.of(dbUser));
+        given(userPasswordManager.encryptUserPassword(dbUser)).willReturn(PASSWORD2);
+        given(authMailSenderImpl.sendMail(anyString(), anyString(), anyString())).willReturn(Boolean.TRUE);
+
+        //when
+        authService.sendUserPasswordEmail(EMAIL1);
+
+        //then
+        assertThat(dbUser.getPassword()).isNotEqualTo(PASSWORD2);
+    }
+
+    @Test
+    @DisplayName("패스워드 찾기 요청 시, 요청받은 이메일을 가진 유저가 존재한다면 true를 반환한다.")
+    public void sendUserPasswordMail_success2() {
+        //given
+        User dbUser = User.builder()
+                .email(EMAIL1)
+                .password(PASSWORD1)
+                .build();
+
+        given(userRepository.findUserByEmail(EMAIL1)).willReturn(Optional.of(dbUser));
+        given(userPasswordManager.encryptUserPassword(dbUser)).willReturn(PASSWORD2);
+        given(authMailSenderImpl.sendMail(anyString(), anyString(), anyString())).willReturn(Boolean.TRUE);
+
+        //when
+        Boolean result = authService.sendUserPasswordEmail(EMAIL1);
 
         //then
         assertThat(result).isTrue();
