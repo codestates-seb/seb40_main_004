e108d4d3188027383397649fc5a9ed778922e28e
시온백 <tldhs05222@naver.com>
2022-11-12 05:27:29 +0900

    test : requestUpdateArticleToEntity 매퍼 작동 테스트 성공

---
+++ server/morak_back_end/src/test/java/com/morakmorak/morak_back_end/mapper/ArticleMapperTest.java
@@ -0,0 +1,39 @@
+package com.morakmorak.morak_back_end.mapper;
+
+import com.morakmorak.morak_back_end.dto.ArticleDto;
+import com.morakmorak.morak_back_end.dto.UserDto;
+import com.morakmorak.morak_back_end.entity.Article;
+import com.morakmorak.morak_back_end.util.ArticleTestConstants;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ArticleMapperTest {
+    @Autowired
+    ArticleMapper articleMapper;
+
+    @Test
+    @DisplayName("requestUpdateArticleToEntity 매퍼 작동 테스트")
+    public void requestUpdateArticle함ToEntityTest() throws Exception{
+        //given
+        ArticleDto.RequestUpdateArticle requestUpdateArticle = ArticleTestConstants.REQUEST_UPDATE_ARTICLE;
+        UserDto.UserInfo userInfo = UserDto.UserInfo.builder()
+                .id(1L).email("tldjs@naver.com").roles(List.of("ROLE_USER")).build();
+        //when
+        Article article = articleMapper.requestUpdateArticleToEntity(requestUpdateArticle, userInfo);
+        //then
+        assertThat(article.getId()).isEqualTo(1L);
+        assertThat(article.getContent()).isEqualTo(requestUpdateArticle.getContent());
+        assertThat(article.getThumbnail()).isEqualTo(requestUpdateArticle.getThumbnail());
+
+     }
+}
