60b569f7d1c37cf325bfd17471a5a347e5137bac
시온백 <tldhs05222@naver.com>
2022-11-13 03:29:21 +0900

    feat : ArticleController 통합테스트를 진행하기 위한 repository 기능 구현

--- server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/repository/ArticleRepository.java
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/repository/ArticleRepository.java
@@ -3,7 +3,11 @@ package com.morakmorak.morak_back_end.repository;
 
 import com.morakmorak.morak_back_end.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.expression.spel.ast.OpAnd;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-
+    Optional<Article> findArticleByContent(String content);
 }
