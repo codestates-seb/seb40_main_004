9949897f48d98e3e05345b8c5f17676485bd94ee
HyeonWooGa <yeonwoopark22@gmail.com>
2022-11-14 08:52:52 +0900

    update : pages/index 업데이트
    기본 Header 적용과 메인 페이지를 알리는 텍스트입니다.

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,14 @@
-import type { NextPage } from 'next';
+import { NextPage } from 'next';
+import { Header } from '../components/common/Header';
+
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl text-orange-400">
-      {false ? true : true ? 'Hello World!' : false}
-    </h1>
+    <>
+      <main className="max-w-[1280px] mx-auto">
+        <Header />
+        <h1>Main</h1>
+      </main>
+    </>
   );
 };
 
