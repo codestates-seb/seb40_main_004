db468845f3d2620e3154ca611219dac67934bc1d
HyeonWooGa <yeonwoopark22@gmail.com>
2022-11-14 08:54:45 +0900

    feat : pages/edit-password/index 생성
    기본 Header 적용과 해당 페이지를 알리는 텍스트입니다.

---
+++ client/pages/edit-password/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from 'next';
+import { Header } from '../../components/common/Header';
+
+const EditPassword: NextPage = () => {
+  return (
+    <>
+      <Header />
+      <main className="max-w-[1280px] mx-auto">
+        <h1>Edit-Password</h1>
+      </main>
+    </>
+  );
+};
+
+export default EditPassword;
