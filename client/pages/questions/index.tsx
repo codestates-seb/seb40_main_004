29fadf152686dca600d16262e695ab2270309e32
HyeonWooGa <yeonwoopark22@gmail.com>
2022-11-14 08:55:29 +0900

    feat : pages/questions/index 생성
    기본 Header 적용과 해당 페이지를 알리는 텍스트입니다.

---
+++ client/pages/questions/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from 'next';
+import { Header } from '../../components/common/Header';
+
+const Questions: NextPage = () => {
+  return (
+    <>
+      <Header />
+      <main className="max-w-[1280px] mx-auto">
+        <h1>Questions</h1>
+      </main>
+    </>
+  );
+};
+
+export default Questions;
