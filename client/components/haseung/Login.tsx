f0cdcb240b8fb2097b14a5b9e830e256902b981e
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-11-14 13:06:44 +0900

    style : Login  컴포넌트 수정 - react hook form을 통한 간단한 Validation 적용(추후 더 적용 예정) - input은 LoginInput.tsx로 분리

---
+++ client/components/haseung/Login.tsx
@@ -0,0 +1,19 @@
+/*
+ * 책임 작성자: 정하승
+ * 최초 작성일: 2022-11-14
+ * 최근 수정일: 2022-11-14
+ */
+
+import LoginInput from './LoginInput';
+
+export const Login = () => {
+  return (
+    <main className="flex justify-center text-lg">
+      <article className="text-center">
+        <h3 className="font-bold">로그인</h3>
+        <h3 className="mt-5">다시 오셨군요! 환영합니다.</h3>
+        <LoginInput />
+      </article>
+    </main>
+  );
+};
---
+++ client/components/haseung/LoginInput.tsx
@@ -0,0 +1,56 @@
+/*
+ * 책임 작성자: 정하승
+ * 최초 작성일: 2022-11-14
+ * 최근 수정일: 2022-11-14
+ */
+
+import { useForm } from 'react-hook-form';
+
+type LoginForm = {
+  email: string;
+  password: string;
+  newPassword: string;
+};
+
+const LoginInput = () => {
+  const onValid = ({ email, password, newPassword }: LoginForm) => {
+    console.log(email, password, newPassword);
+  };
+  const { register, handleSubmit } = useForm<LoginForm>();
+  return (
+    <form
+      className="flex flex-col mx-auto justify-center items-start mt-10"
+      onSubmit={handleSubmit(onValid)}
+    >
+      <label>이메일</label>
+      <input
+        {...register('email', { required: true })}
+        className="rounded-full w-96 h-10 border border-font-gray placeholder:text-base placeholder:pl-3 placeholder:pb-2"
+        type="text"
+        placeholder="hyejj19@naver.com"
+      />
+      <label>비밀번호</label>
+      <input
+        {...register('password', { required: true })}
+        className="rounded-full border w-96 h-10 border-font-gray"
+        type="password"
+        autoComplete="off"
+      />
+      <label>비밀번호 확인</label>
+      <input
+        {...register('newPassword', { required: true })}
+        className="rounded-full border w-96 h-10 border-font-gray"
+        type="password"
+        autoComplete="off"
+      />
+      <button
+        className="mx-auto mt-4 bg-main-yellow rounded-full w-96 h-10 hover:bg-main-orange"
+        type="submit"
+      >
+        로그인
+      </button>
+    </form>
+  );
+};
+
+export default LoginInput;
