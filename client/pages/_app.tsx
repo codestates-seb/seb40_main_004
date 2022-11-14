bd7fd77d31e214de6bc88d078159916d398cf15b
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-11-14 14:38:13 +0900

    docs : 주석 추가

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,14 +2,17 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import 'react-quill/dist/quill.snow.css';
 import { RecoilRoot } from 'recoil';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <div className="bg-background-gray text-font-gray min-h-screen">
-        <Component {...pageProps} />
-      </div>
-    </RecoilRoot>
+    <>
+      <RecoilRoot>
+        <div className="bg-background-gray text-font-gray min-h-screen">
+          <Component {...pageProps} />
+        </div>
+      </RecoilRoot>
+    </>
   );
 }
 
