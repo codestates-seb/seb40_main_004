4c0ac22e42dfed491185ef38f179d11abc61f7ad
Hyejung Park <hyejj19@naver.com>
2022-11-15 16:35:05 +0900

    chore : 폴더 이동 및 불필요한 컴포넌트 삭제

--- client/components/hyejung/QuestionContent/QuestionWriterBtns.tsx
+++
@@ -1,8 +0,0 @@
-export const QuestionWriterBtns = () => {
-  return (
-    <article className="space-x-2 text-sm w-[80px] flex justify-end">
-      <button>수정</button>
-      <button>삭제</button>
-    </article>
-  );
-};
