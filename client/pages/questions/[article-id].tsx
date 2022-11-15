cdaf41cd8ab4d2c713b88971c9c1f44955dd4a1a
Hyejung Park <hyejj19@naver.com>
2022-11-15 16:41:34 +0900

    style : 질문 상세페이지 QuestionAnswer 컴포넌트 추가

--- client/pages/questions/[article-id].tsx
+++ client/pages/questions/[article-id].tsx
@@ -8,13 +8,21 @@ import { NextPage } from 'next';
 import { Header } from '../../components/common/Header';
 import { Footer } from '../../components/common/Footer';
 import { QuestionContent } from '../../components/hyejung/QuestionContent/QuestionContent';
+import { QusetionAnswer } from '../../components/hyejung/QuestionAnswer';
 
 const QuestionDetail: NextPage = () => {
   return (
     <>
       <Header />
-      <main className="max-w-[1280px] mx-auto min-h-[80vh] bg-white">
+      <main className="max-w-[1280px] mx-auto min-h-[80vh] bg-white p-[60px] ">
         <QuestionContent />
+        <section className="flex w-full text-lg sm:text-xl space-x-2 items-center">
+          <h1 className="text-main-yellow font-semibold text-2xl sm:text-3xl">
+            A.
+          </h1>
+          <h1>1개의 답변이 달렸습니다.</h1>
+        </section>
+        <QusetionAnswer />
       </main>
       <Footer />
     </>
