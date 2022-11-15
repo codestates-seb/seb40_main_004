669983a51dfe49090baf35eb4ade8d292cb24be7
Hyejung Park <hyejj19@naver.com>
2022-11-15 16:41:11 +0900

    feat : 코멘트 및 후기 메시지 작성을 위한 TextArea 컴포넌트 생성

---
+++ client/components/hyejung/TextArea.tsx
@@ -0,0 +1,14 @@
+import { Button } from '../common/Button';
+export const TextArea = () => {
+  return (
+    <div className="flex flex-col space-y-3">
+      <textarea
+        className="w-full rounded-[20px] border p-4 text-sm  focus:outline-main-gray"
+        rows={4}
+      />
+      <div className="flex justify-end">
+        <Button>코멘트 등록</Button>
+      </div>
+    </div>
+  );
+};
