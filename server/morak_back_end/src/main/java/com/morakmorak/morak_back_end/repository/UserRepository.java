271261237cf52d3864517fc819e341dd28934b9c
yangddoddi <pud1251@gmail.com>
2022-11-16 21:02:11 +0900

    feat : userRepository 추가

--- server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/repository/UserRepository.java
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.morakmorak.morak_back_end.repository;
 
+import com.morakmorak.morak_back_end.dto.ActivityDto;
 import com.morakmorak.morak_back_end.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
