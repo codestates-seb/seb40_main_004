886b0d7547c643297ec2206119d2943ec48f0054
yangddoddi <pud1251@gmail.com>
2022-11-16 20:48:48 +0900

    feat : ReviewBadgeDto 반환 형태 추가

--- server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/dto/ReviewBadgeDto.java
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/dto/ReviewBadgeDto.java
@@ -1,4 +1,23 @@
 package com.morakmorak.morak_back_end.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+
 public class ReviewBadgeDto {
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class SimpleReviewBadgeDto {
+        @NotBlank
+        private Long reviewBadgeId;
+        private String name;
+
+        @QueryProjection
+        public SimpleReviewBadgeDto(Long reviewBadgeId, String name) {
+            this.reviewBadgeId = reviewBadgeId;
+            this.name = name;
+        }
+    }
 }
