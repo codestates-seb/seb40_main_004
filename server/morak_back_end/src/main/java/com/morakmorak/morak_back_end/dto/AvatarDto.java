e6467730a14a12c4dc1a1680da63e7492eb4fc38
yangddoddi <pud1251@gmail.com>
2022-11-16 20:48:28 +0900

    feat : Avatar Dto 반환 형태 추가

--- server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/dto/AvatarDto.java
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/dto/AvatarDto.java
@@ -1,15 +1,22 @@
 package com.morakmorak.morak_back_end.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.*;
 
 public class AvatarDto {
     @Getter
     @Builder
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    @AllArgsConstructor(access = AccessLevel.PROTECTED)
     public static class Request {
         private Long avatarId;
         private String remotePath;
         private String filename;
+
+        @QueryProjection
+        public Request(Long avatarId, String remotePath, String filename) {
+            this.avatarId = avatarId;
+            this.remotePath = remotePath;
+            this.filename = filename;
+        }
     }
 }
