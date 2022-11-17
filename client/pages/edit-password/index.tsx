7655a6e9e67376d15bb173294faa0368af915cf0
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-11-17 23:12:01 +0900

    style : 비밀번호 변경 페이지 작업

--- client/pages/edit-password/index.tsx
+++ client/pages/edit-password/index.tsx
@@ -6,13 +6,20 @@
 
 import { NextPage } from 'next';
 import { Header } from '../../components/common/Header';
+import { EditPasswordForm } from '../../components/haseung/EditPasswordForm';
 
 const EditPassword: NextPage = () => {
   return (
     <>
       <Header />
       <main className="max-w-[1280px] mx-auto">
-        <h1>Edit-Password</h1>
+        <article className="text-center mt-10">
+          <h3 className="font-bold">비밀번호 변경하기</h3>
+          <h3 className="mt-5">새로운 비밀번호로 변경합니다.</h3>
+          <section className="flex justify-center text-lg">
+            <EditPasswordForm />
+          </section>
+        </article>
       </main>
     </>
   );
