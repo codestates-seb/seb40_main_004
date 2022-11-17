a653770ba92dc8399be421dad10eeb307573fb4c
시온백 <tldhs05222@naver.com>
2022-11-17 21:30:37 +0900

    feat : CommentMapper commentToCommentDto 매핑 로직 구현

---
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/mapper/CommentMapper.java
@@ -0,0 +1,18 @@
+package com.morakmorak.morak_back_end.mapper;
+
+import com.morakmorak.morak_back_end.dto.CommentDto;
+import com.morakmorak.morak_back_end.entity.Comment;
+import org.mapstruct.Mapping;
+
+public interface CommentMapper {
+
+    @Mapping(source = "comment.id", target = "commentId")
+    @Mapping(source = "comment.article.id", target = "articleId")
+    @Mapping(source = "comment.user.id", target = "userInfo.userId")
+    @Mapping(source = "comment.user.nickname", target = "userInfo.nickname")
+    @Mapping(source = "comment.user.grade", target = "userInfo.grade")
+    @Mapping(source = "comment.user.avatar.id", target = "avatar.avatarId")
+    @Mapping(source = "comment.user.avatar.originalFileName", target = "avatar.fileName")
+    @Mapping(source = "comment.user.avatar.remotePath", target = "avatar.remotePath")
+    CommentDto.Response commentToCommentDto(Comment comment);
+}
