efd35f6f7d8f6a1e83e017491a5f2efc82774fe2
시온백 <tldhs05222@naver.com>
2022-11-17 21:29:13 +0900

    feat : 북마크 Repository 와 게시글좋아요 Repository 객체 조회 로직

--- server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/repository/ArticleLikeRepository.java
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/repository/ArticleLikeRepository.java
@@ -2,6 +2,13 @@ package com.morakmorak.morak_back_end.repository;
 
 import com.morakmorak.morak_back_end.entity.ArticleLike;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> {
+
+    @Query("select a from ArticleLike a where a.user.id = :userId and a.article.id = :articleId")
+    Optional<ArticleLike> checkUserLiked(Long userId, Long articleId);
+
 }
