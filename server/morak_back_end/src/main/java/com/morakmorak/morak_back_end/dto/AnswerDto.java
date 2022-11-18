99e08eebc37d82d446514979a75551b3de488158
Tldkt <evolym2543@sookmyung.ac.kr>
2022-11-18 23:06:24 +0900

    feat : AnswerDto,AnswerController 구현

---
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/dto/AnswerDto.java
@@ -0,0 +1,46 @@
+package com.morakmorak.morak_back_end.dto;
+
+import com.morakmorak.morak_back_end.entity.Answer;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnswerDto {
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class RequestPostAnswer {
+        @NotBlank
+        @Length(min = 10)
+        private String content;
+        private List<FileDto.RequestFileWithId> fileIdList = new ArrayList<>();
+//        private Long thumbnail; todo: 반영 여부 확인하기
+    }
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class SimpleResponsePostAnswer {
+        private Long answerId;
+        private UserDto.ResponseSimpleUserDto userInfo;
+        private AvatarDto.SimpleResponse avatar;
+        private String content;
+        private LocalDateTime createdAt;
+        private LocalDateTime lastModifiedAt;
+        public static SimpleResponsePostAnswer of(Answer savedAnswer) {
+            return SimpleResponsePostAnswer.builder()
+                    .answerId(savedAnswer.getId())
+                    .userInfo(UserDto.ResponseSimpleUserDto.of(savedAnswer.getUser()))
+                    .avatar(AvatarDto.SimpleResponse.of(savedAnswer.getUser().getAvatar()))
+                    .content(savedAnswer.getContent())
+                    .createdAt(savedAnswer.getCreatedAt())
+                    .lastModifiedAt(savedAnswer.getLastModifiedAt())
+                    .build();
+        }
+    }
+}
