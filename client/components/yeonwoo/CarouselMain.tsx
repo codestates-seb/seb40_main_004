ff69c9ff16395e8f0c18c68bdcb5d7d9ff399306
HyeonWooGa <yeonwoopark22@gmail.com>
2022-11-19 01:23:14 +0900

    feat : CarouselMain 레이아웃
    자동 넘김 5초 적용

---
+++ client/components/yeonwoo/CarouselMain.tsx
@@ -0,0 +1,121 @@
+/*
+ * 책임 작성자: 박연우
+ * 최초 작성일: 2022-11-18
+ * 최근 수정일: 2022-11-18
+ */
+
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import { wrap } from 'popmotion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faChevronLeft,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
+
+const images = [
+  'https://d33wubrfki0l68.cloudfront.net/dd23708ebc4053551bb33e18b7174e73b6e1710b/dea24/static/images/wallpapers/shared-colors@2x.png',
+  'https://d33wubrfki0l68.cloudfront.net/49de349d12db851952c5556f3c637ca772745316/cfc56/static/images/wallpapers/bridge-02@2x.png',
+  'https://d33wubrfki0l68.cloudfront.net/594de66469079c21fc54c14db0591305a1198dd6/3f4b1/static/images/wallpapers/bridge-01@2x.png',
+];
+
+const variants = {
+  enter: (direction: number) => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction: number) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+};
+
+/**
+ * Experimenting with distilling swipe offset and velocity into a single variable, so the
+ * less distance a user has swiped, the more velocity they need to register as a swipe.
+ * Should accomodate longer swipes and short flicks without having binary checks on
+ * just distance thresholds and velocity > 0.
+ */
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset: number, velocity: number) => {
+  return Math.abs(offset) * velocity;
+};
+
+export const CarouselMain = () => {
+  const [[page, direction], setPage] = useState([0, 0]);
+
+  // We only have 3 images, but we paginate them absolutely (ie 1, 2, 3, 4, 5...) and
+  // then wrap that within 0-2 to find our image ID in the array below. By passing an
+  // absolute page index as the `motion` component's `key` prop, `AnimatePresence` will
+  // detect it as an entirely new image. So you can infinitely paginate as few as 1 images.
+  const imageIndex = wrap(0, images.length, page);
+
+  const paginate = (newDirection: number) => {
+    setPage([page + newDirection, newDirection]);
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      paginate(1);
+    }, 5000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [page]);
+
+  return (
+    <>
+      <AnimatePresence initial={false} custom={direction}>
+        <motion.img
+          key={page}
+          className="min-w-[1280px] h-[350px] absolute"
+          src={images[imageIndex]}
+          custom={direction}
+          variants={variants}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          transition={{
+            x: { type: 'spring', stiffness: 300, damping: 30 },
+          }}
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          dragElastic={1}
+          onDragEnd={(e, { offset, velocity }) => {
+            const swipe = swipePower(offset.x, velocity.x);
+
+            if (swipe < -swipeConfidenceThreshold) {
+              paginate(1);
+            } else if (swipe > swipeConfidenceThreshold) {
+              paginate(-1);
+            }
+          }}
+        />
+      </AnimatePresence>
+      <div
+        className="absolute top-[50% - 20px] z-10 right-8 hover:cursor-pointer"
+        onClick={() => paginate(1)}
+      >
+        <FontAwesomeIcon icon={faChevronRight} size="4x" />
+      </div>
+      <div
+        className="absolute top-[50% - 20px] z-10 left-8 hover:cursor-pointer"
+        onClick={() => paginate(-1)}
+      >
+        <FontAwesomeIcon icon={faChevronLeft} size="4x" />
+      </div>
+    </>
+  );
+};
