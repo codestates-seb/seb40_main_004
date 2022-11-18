6dd01633f24ab6cbe2e8de1eb888741f2d1c0545
HyeonWooGa <yeonwoopark22@gmail.com>
2022-11-19 01:24:30 +0900

    feat : HallOfFame 레이아웃

---
+++ client/components/yeonwoo/HallOfFame.tsx
@@ -0,0 +1,178 @@
+/*
+ * 책임 작성자: 박연우
+ * 최초 작성일: 2022-11-18
+ * 최근 수정일: 2022-11-18
+ */
+
+import Image from 'next/image';
+
+export const HallOfFame = () => {
+  return (
+    <>
+      <div className="mb-10">
+        <span className="text-2xl font-bold">🏆 명예의 전당</span>
+      </div>
+      <div>
+        <div className="w-[350px] h-[60px] flex gap-[10px] items-center border-b mt-4">
+          <div className="w-[14px]">
+            <span className="text-lg font-bold">1.</span>
+          </div>
+          <div className="w-[326px] flex gap-2">
+            <Image src="/favicon.ico" width="45px" height="45px" />
+            <div>
+              <div>
+                <span className="text-[15px] font-bold">김코딩</span>
+              </div>
+              <div className="flex justify-between w-[273px]">
+                <div>
+                  <span className="text-sm">오늘도 좋은 하루~!</span>
+                </div>
+                <div>
+                  <span className="text-xs mr-2">💎 다이아</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="w-[350px] h-[60px] flex gap-[10px] items-center border-b mt-4">
+          <div className="w-[14px]">
+            <span className="text-lg font-bold">2.</span>
+          </div>
+          <div className="w-[326px] flex gap-2">
+            <Image src="/favicon.ico" width="45px" height="45px" />
+            <div>
+              <div>
+                <span className="text-[15px] font-bold">김코딩</span>
+              </div>
+              <div className="flex justify-between w-[273px]">
+                <div>
+                  <span className="text-sm">오늘도 좋은 하루~!</span>
+                </div>
+                <div>
+                  <span className="text-xs mr-2">💎 다이아</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="w-[350px] h-[60px] flex gap-[10px] items-center border-b mt-4">
+          <div className="w-[14px]">
+            <span className="text-lg font-bold">3.</span>
+          </div>
+          <div className="w-[326px] flex gap-2">
+            <Image src="/favicon.ico" width="45px" height="45px" />
+            <div>
+              <div>
+                <span className="text-[15px] font-bold">김코딩</span>
+              </div>
+              <div className="flex justify-between w-[273px]">
+                <div>
+                  <span className="text-sm">오늘도 좋은 하루~!</span>
+                </div>
+                <div>
+                  <span className="text-xs mr-2">💎 다이아</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="w-[350px] h-[60px] flex gap-[10px] items-center border-b mt-4">
+          <div className="w-[14px]">
+            <span className="text-lg font-bold">4.</span>
+          </div>
+          <div className="w-[326px] flex gap-2">
+            <Image src="/favicon.ico" width="45px" height="45px" />
+            <div>
+              <div>
+                <span className="text-[15px] font-bold">김코딩</span>
+              </div>
+              <div className="flex justify-between w-[273px]">
+                <div>
+                  <span className="text-sm">오늘도 좋은 하루~!</span>
+                </div>
+                <div>
+                  <span className="text-xs mr-2">💎 다이아</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="w-[350px] h-[60px] flex gap-[10px] items-center border-b mt-4">
+          <div className="w-[14px]">
+            <span className="text-lg font-bold">5.</span>
+          </div>
+          <div className="w-[326px] flex gap-2">
+            <Image src="/favicon.ico" width="45px" height="45px" />
+            <div>
+              <div>
+                <span className="text-[15px] font-bold">김코딩</span>
+              </div>
+              <div className="flex justify-between w-[273px]">
+                <div>
+                  <span className="text-sm">오늘도 좋은 하루~!</span>
+                </div>
+                <div>
+                  <span className="text-xs mr-2">💎 다이아</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="w-[350px] h-[60px] flex gap-[10px] items-center border-b mt-4">
+          <div className="w-[14px]">
+            <span className="text-lg font-bold">6.</span>
+          </div>
+          <div className="w-[326px] flex gap-2">
+            <Image src="/favicon.ico" width="45px" height="45px" />
+            <div>
+              <div>
+                <span className="text-[15px] font-bold">김코딩</span>
+              </div>
+              <div className="flex justify-between w-[273px]">
+                <div>
+                  <span className="text-sm">오늘도 좋은 하루~!</span>
+                </div>
+                <div>
+                  <span className="text-xs mr-2">💎 다이아</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="w-[350px] h-[60px] flex gap-[10px] items-center border-b mt-4">
+          <div className="w-[14px]">
+            <span className="text-lg font-bold">7.</span>
+          </div>
+          <div className="w-[326px] flex gap-2">
+            <Image src="/favicon.ico" width="45px" height="45px" />
+            <div>
+              <div>
+                <span className="text-[15px] font-bold">김코딩</span>
+              </div>
+              <div className="flex justify-between w-[273px]">
+                <div>
+                  <span className="text-sm">오늘도 좋은 하루~!</span>
+                </div>
+                <div>
+                  <span className="text-xs mr-2">💎 다이아</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
