2f98b135b3b919d0cff88716f2018011a38324ce
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-11-19 22:01:50 +0900

    styles : font 적용 부분 코드 임시 삭제

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -4,12 +4,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="ko">
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
