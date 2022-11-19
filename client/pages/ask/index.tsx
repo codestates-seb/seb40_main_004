2958233cae8beb4f5e3169cd3a51b13e204ab8cd
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-11-19 12:09:51 +0900

    rename : 폴더명 변경
    - articles -> ask로 변경

---
+++ client/pages/ask/index.tsx
@@ -0,0 +1,13 @@
+import { Editor } from '../../components/common/Editor';
+import { TagSelect } from '../../components/haseung/TagSelect';
+
+const index = () => {
+  return (
+    <>
+      <Editor />
+      <TagSelect />
+    </>
+  );
+};
+
+export default index;
