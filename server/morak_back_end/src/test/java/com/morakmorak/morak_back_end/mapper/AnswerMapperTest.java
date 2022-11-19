0a0bf2065be4b8fa3280f8f70bc8359ce901cd2c
시온백 <tldhs05222@naver.com>
2022-11-20 03:01:25 +0900

    test : 답변글 좋아요 기능 AnswerMapper.makingResponseAnswerLikeDto 메서드 작동 테스트 완료

---
+++ server/morak_back_end/src/test/java/com/morakmorak/morak_back_end/mapper/AnswerMapperTest.java
@@ -0,0 +1,40 @@
+package com.morakmorak.morak_back_end.mapper;
+
+import com.morakmorak.morak_back_end.dto.AnswerDto;
+
+import com.morakmorak.morak_back_end.dto.UserDto;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class AnswerMapperTest {
+
+    @Autowired
+    AnswerMapper answerMapper;
+
+
+    @Test
+    @DisplayName("makingResponseAnswerLikeDto 메퍼 작동 테스트")
+    public void makingResponseAnswerLikeDto(){
+        //given
+        Long answerId = 1L;
+        UserDto.UserInfo userInfo = UserDto.UserInfo.builder().id(1L).build();
+        //when
+        AnswerDto.ResponseAnswerLike responseAnswerLike =
+                answerMapper.makingResponseAnswerLikeDto(answerId, userInfo.getId(), true, 1);
+        //then
+        assertThat(responseAnswerLike.getAnswerId()).isEqualTo(1L);
+        assertThat(responseAnswerLike.getUserId()).isEqualTo(1L);
+        assertThat(responseAnswerLike.getIsLiked()).isTrue();
+        assertThat(responseAnswerLike.getLikeCount()).isEqualTo(1);
+    }
+}
