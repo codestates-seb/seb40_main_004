fdc3e4a6dc307c6b9549e42cdcf0c9ba6c9807d1
시온백 <tldhs05222@naver.com>
2022-11-20 00:52:54 +0900

    test : 게시글 좋아요 서비스 로직 테스트 완료

--- server/morak_back_end/src/test/java/com/morakmorak/morak_back_end/service/ArticleServiceTest.java
+++ server/morak_back_end/src/test/java/com/morakmorak/morak_back_end/service/ArticleServiceTest.java
@@ -6,10 +6,7 @@ import com.morakmorak.morak_back_end.entity.enums.TagName;
 import com.morakmorak.morak_back_end.exception.BusinessLogicException;
 import com.morakmorak.morak_back_end.exception.ErrorCode;
 import com.morakmorak.morak_back_end.mapper.ArticleMapper;
-import com.morakmorak.morak_back_end.repository.ArticleRepository;
-import com.morakmorak.morak_back_end.repository.CategoryRepository;
-import com.morakmorak.morak_back_end.repository.FileRepository;
-import com.morakmorak.morak_back_end.repository.TagRepository;
+import com.morakmorak.morak_back_end.repository.*;
 import com.morakmorak.morak_back_end.util.ArticleTestConstants;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -48,6 +45,8 @@ public class ArticleServiceTest {
     @Mock
     ArticleRepository articleRepository;
 
+    @Mock
+    ArticleLikeRepository articleLikeRepository;
 
     @Test
     @DisplayName("게시글 등록 서비스로직 성공 테스트")
@@ -177,4 +176,96 @@ public class ArticleServiceTest {
            assertThat(aBoolean).isTrue();
         }
 
+        @Test
+        @DisplayName("게시글의 좋아요를 누를때 회원이 좋아요를 처음누르는거면 201코드와 json을 리턴한다.")
+        public void pressLikeButton_suc(){
+        //given
+            Long articleId = 1L;
+            UserDto.UserInfo userInfo = UserDto.UserInfo.builder().id(1L).build();
+            ArticleLike articleLike = ArticleLike.builder().build();
+
+            Article dbArticle = Article.builder().id(1L).articleLikes(List.of(articleLike)).build();
+
+            ArticleDto.ResponseArticleLike responseArticleLike =
+                    ArticleDto.ResponseArticleLike.builder().userId(1L)
+                            .articleId(1L).isLiked(true).likeCount(1).build();
+
+            given(articleRepository.findById(anyLong())).willReturn(Optional.of(dbArticle));
+            given(userService.findVerifiedUserById(anyLong())).willReturn(User.builder().id(1L).build());
+            given(articleLikeRepository.checkUserLiked(1L, 1L)).willReturn(Optional.empty());
+            given(articleLikeRepository.checkUserLiked(1L, 1L)).willReturn(Optional.of(ArticleLike.builder().build()));
+            given(articleMapper.makingResponseArticleLikeDto(articleId, userInfo.getId(), Boolean.TRUE, 1))
+                    .willReturn(responseArticleLike);
+
+        //when
+            ArticleDto.ResponseArticleLike result = articleService.pressLikeButton(articleId, userInfo);
+            //then
+            assertThat(result.getLikeCount()).isEqualTo(1);
+            assertThat(result.getIsLiked()).isTrue();
+            assertThat(result.getArticleId()).isEqualTo(articleId);
+            assertThat(result.getUserId()).isEqualTo(userInfo.getId());
+        }
+
+        @Test
+        @DisplayName("게시글의 좋아요를 누를때 회원이 좋아요를 두번째 누르는거면 (취소) 201코드와 json을 리턴한다.")
+        public void pressLikeButton_suc2(){
+        //given
+            Long articleId = 1L;
+            UserDto.UserInfo userInfo = UserDto.UserInfo.builder().id(1L).build();
+
+            Article dbArticle = Article.builder().id(1L).build();
+
+            ArticleDto.ResponseArticleLike responseArticleLike =
+                    ArticleDto.ResponseArticleLike.builder().userId(1L)
+                            .articleId(1L).isLiked(false).likeCount(0).build();
+
+            given(articleRepository.findById(anyLong())).willReturn(Optional.of(dbArticle));
+            given(userService.findVerifiedUserById(anyLong())).willReturn(User.builder().id(1L).build());
+            given(articleLikeRepository.checkUserLiked(1L, 1L)).willReturn(Optional.empty());
+            given(articleLikeRepository.checkUserLiked(1L, 1L)).willReturn(Optional.of(ArticleLike.builder().build()));
+            given(articleMapper.makingResponseArticleLikeDto(articleId, userInfo.getId(), Boolean.TRUE, 0))
+                    .willReturn(responseArticleLike);
+
+            //when
+            ArticleDto.ResponseArticleLike result = articleService.pressLikeButton(articleId, userInfo);
+            //then
+            assertThat(result.getLikeCount()).isEqualTo(0);
+            assertThat(result.getIsLiked()).isFalse();
+            assertThat(result.getArticleId()).isEqualTo(articleId);
+            assertThat(result.getUserId()).isEqualTo(userInfo.getId());
+        }
+
+    @Test
+    @DisplayName("게시글의 좋아요를 해당 게시글이 존재하지 않을경우 Article Not Found를 던지고 404 예외코드를 던진다.")
+    public void pressLikeButton_fail1(){
+        //given
+        Long articleId = 1L;
+        UserDto.UserInfo userInfo = UserDto.UserInfo.builder().id(1L).build();
+
+        given(articleRepository.findById(anyLong()))
+                .willThrow(new BusinessLogicException(ErrorCode.ARTICLE_NOT_FOUND));
+
+        //when
+        //then
+        assertThatThrownBy(() -> articleService.pressLikeButton(articleId, userInfo))
+                .isInstanceOf(BusinessLogicException.class);
+
+    }
+    @Test
+    @DisplayName("게시글의 좋아요를 로그인한 회원이 아닐경우 User Not Found를 던지고 404 예외코드를 던진다.")
+    public void pressLikeButton_fail2(){
+        //given
+        Long articleId = 1L;
+        UserDto.UserInfo userInfo = UserDto.UserInfo.builder().id(1L).build();
+        Article dbArticle = Article.builder().id(1L).build();
+
+        given(articleRepository.findById(anyLong())).willReturn(Optional.of(dbArticle));
+        given(userService.findVerifiedUserById(anyLong()))
+                .willThrow(new BusinessLogicException(ErrorCode.USER_NOT_FOUND));
+        //when
+        //then
+        assertThatThrownBy(() -> articleService.pressLikeButton(articleId, userInfo))
+                .isInstanceOf(BusinessLogicException.class);
+
+    }
 }
