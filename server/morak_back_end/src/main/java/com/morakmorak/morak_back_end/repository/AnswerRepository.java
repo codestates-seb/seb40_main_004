ecbc087d255745c0110df68fc57d43f5d884bc91
시온백 <tldhs05222@naver.com>
2022-11-20 02:55:24 +0900

    feat : 답변글 좋아요 기능 테스트를 위한 레퍼지토리 로직 구현

--- server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/repository/AnswerRepository.java
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/repository/AnswerRepository.java
@@ -3,5 +3,8 @@ package com.morakmorak.morak_back_end.repository;
 import com.morakmorak.morak_back_end.entity.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    Optional<Answer> findAnswerByContent(String content);
 }
