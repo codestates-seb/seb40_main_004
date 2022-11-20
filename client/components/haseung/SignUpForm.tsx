4c64e72c149a74b42d83408b9ea43109d35919e0
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-11-20 18:50:46 +0900

    feat : 회원가입 폼에 대한 패턴 추가 - 정규식 패턴 validation 추가 - 에러메시지 추가

--- client/components/haseung/SignUpForm.tsx
+++ client/components/haseung/SignUpForm.tsx
@@ -7,6 +7,7 @@
 
 import axios from 'axios';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { Button } from '../common/Button';
 import { Divider } from './Divider';
@@ -20,9 +21,11 @@ type SignUpProps = {
 };
 
 export const SignUpForm = () => {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors },
   } = useForm<SignUpProps>();
 
@@ -32,6 +35,15 @@ export const SignUpForm = () => {
     confirmPassword,
     nickName,
   }) => {
+    if (password !== confirmPassword) {
+      console.log('비밀번호 다름');
+      setError(
+        'confirmPassword',
+        { message: '비밀번호가 맞지 않습니다.' },
+        { shouldFocus: true },
+      );
+    }
+
     axios
       .post('http://localhost:3000/users', {
         email,
@@ -39,7 +51,10 @@ export const SignUpForm = () => {
         confirmPassword,
         nickName,
       })
-      .then((res) => res)
+      .then((res) => {
+        // console.log(res);
+        router.push('/authentication-number');
+      })
       .catch((error) => {
         console.log(error);
       });
@@ -54,47 +69,72 @@ export const SignUpForm = () => {
         닉네임
       </label>
       <input
-        {...register('nickName', { required: true })}
+        {...register('nickName', {
+          required: true,
+          minLength: {
+            value: 2,
+            message: '닉네임은 2자 이상이어야 합니다.',
+          },
+          maxLength: {
+            value: 16,
+            message: '닉네임은 16자 이하이어야 합니다.',
+          },
+        })}
         className="rounded-full w-96 h-10 placeholder:text-base placeholder:pl-3 placeholder:pb-2"
         type="text"
         placeholder="닉네임을 입력해주세요."
       />
-      {errors.nickName?.message ? '닉네임을 입력해주세요.' : ''}
+      <p className="font-bold text-red-500">{errors.nickName?.message}</p>
       <label htmlFor="email" className="font-bold">
         이메일
       </label>
       <input
-        {...register('email', { required: true })}
+        {...register('email', {
+          required: true,
+          pattern: {
+            value: /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i,
+            message: '이메일이 형식에 맞지 않습니다.',
+          },
+        })}
         className="rounded-full w-96 h-10 placeholder:text-base placeholder:pl-3 placeholder:pb-2"
         type="text"
         placeholder="이메일을 입력해주세요."
       />
-      {errors.email?.message ? '이메일은 필수 항목입니다.' : ''}
+      <p className="text-red-500 font-semibold">{errors.email?.message}</p>
       <label htmlFor="password" className="font-bold">
         비밀번호
       </label>
       <input
-        {...register('password', { required: true })}
+        {...register('password', {
+          required: true,
+          pattern: {
+            value:
+              /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\d~!@#$%^&*()+|=]{8,16}$/i,
+            message: '비밀번호를 확인해주세요.',
+          },
+        })}
         className="rounded-full w-96 h-10"
         type="password"
         autoComplete="off"
       />
-      {errors.password?.message ? '비밀번호를 확인해주세요.' : ''}
+      <p className="font-semibold text-red-500"> {errors.password?.message}</p>
       <label htmlFor="confirmPassword" className="font-bold">
         비밀번호 확인
       </label>
       <input
-        {...register('confirmPassword', { required: true })}
+        {...register('confirmPassword', {
+          required: true,
+        })}
         className="rounded-full w-96 h-10"
         type="password"
         autoComplete="off"
       />
-      {errors.confirmPassword?.message ? '비밀번호가 맞지 않습니다.' : ''}
-      <Link href="/authenticationNumber">
-        <div className="mx-auto rounded-full w-96 h-10 space-y-2">
-          <Button>가입하기</Button>
-        </div>
-      </Link>
+      <p className="font-semibold text-red-500">
+        {errors.confirmPassword?.message}
+      </p>
+      <section className="mx-auto rounded-full w-96 h-10 space-y-2">
+        <input type="submit" value="가입하기" />
+      </section>
       <span className="mt-3 text-main-gray">
         이미 계정이 있으신가요?{' '}
         <Link href="/login">
