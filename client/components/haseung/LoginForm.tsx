efbf61da2d85757e4d5d4280a3d66eb809962b17
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-11-20 21:38:17 +0900

    feat : 로그인 페이지 validation 추가 - 정규식 패턴, 에러 핸들링 추가

--- client/components/haseung/LoginForm.tsx
+++ client/components/haseung/LoginForm.tsx
@@ -1,7 +1,7 @@
 /*
  * 책임 작성자: 정하승
  * 최초 작성일: 2022-11-14
- * 최근 수정일: 2022-11-16
+ * 최근 수정일: 2022-11-20
  */
 
 import axios from 'axios';
@@ -16,20 +16,15 @@ type LoginProps = {
 
 export const LoginForm = () => {
   const onValid = ({ email, password }: LoginProps) => {
-    /* Sending a post request to the server. */
-    // console.log(email, password);
-    // fetch('http://localhost:3000/users', {
-    //   method: 'POST',
-    //   headers: { 'Content-Type': 'application/json' },
-    //   body: JSON.stringify({ email, password }),
-    // })
-    //   .then((res) => res.json())
-    //   .then((data) => console.log('data', data.user));
     axios
       .post('http://localhost:3000/users', { email, password })
       .then((res) => console.log('res', res.data));
   };
-  const { register, handleSubmit } = useForm<LoginProps>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginProps>();
   return (
     <form
       className="flex flex-col mx-auto justify-center items-start mt-10"
@@ -37,18 +32,32 @@ export const LoginForm = () => {
     >
       <label>이메일</label>
       <input
-        {...register('email', { required: true })}
+        {...register('email', {
+          required: true,
+          pattern: {
+            value: /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i,
+            message: '이메일이 형식에 맞지 않습니다.',
+          },
+        })}
         className="rounded-full w-96 h-10 placeholder:text-base placeholder:pl-3 placeholder:pb-2"
         type="text"
         placeholder="hyejj19@naver.com"
       />
       <label>비밀번호</label>
       <input
-        {...register('password', { required: true })}
+        {...register('password', {
+          required: true,
+          pattern: {
+            value:
+              /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\d~!@#$%^&*()+|=]{8,16}$/i,
+            message: '비밀번호를 확인해주세요.',
+          },
+        })}
         className="rounded-full w-96 h-10"
         type="password"
         autoComplete="off"
       />
+      <p className="font-semibold text-red-500">{errors.password?.message}</p>
       <Link href="/edit-password">
         <span className="text-xs mt-3 cursor-pointer hover:text-main-gray">
           비밀번호를 잊어버리셨나요?
