0daaaf6d489a39884161dc5b29158360fb66849b
HyeonWooGa <yeonwoopark22@gmail.com>
2022-11-21 02:24:47 +0900

    feat : edit-profile 페이지 퍼블리싱

--- client/pages/edit-profile/index.tsx
+++ client/pages/edit-profile/index.tsx
@@ -5,15 +5,28 @@
  */
 
 import { NextPage } from 'next';
+import { Footer } from '../../components/common/Footer';
 import { Header } from '../../components/common/Header';
+import { AsideEditProfile } from '../../components/yeonwoo/AsideEditProfile';
+import { EditAvatar } from '../../components/yeonwoo/EditAvatar';
+import { EditProfileComponent } from '../../components/yeonwoo/EditProfile';
 
 const EditProfile: NextPage = () => {
   return (
     <>
       <Header />
-      <main className="max-w-[1280px] mx-auto">
-        <h1>Edit-Profile</h1>
+      <main className="w-[1280px] min-h-screen mx-auto flex justify-between mb-12">
+        <div className="w-[280px] pt-12">
+          <AsideEditProfile />
+        </div>
+        <div className="w-2/4">
+          <EditProfileComponent />
+        </div>
+        <div className="pt-12">
+          <EditAvatar />
+        </div>
       </main>
+      <Footer />
     </>
   );
 };
