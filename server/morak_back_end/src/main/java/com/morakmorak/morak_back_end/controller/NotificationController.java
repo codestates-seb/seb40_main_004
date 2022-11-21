b2c5177b08b54f59be08815d14ebc0bcb8be60a6
yangddoddi <pud1251@gmail.com>
2022-11-22 02:12:10 +0900

    update : 알림 리다이렉트 로직 컨트롤러 계충 구현

--- server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/controller/NotificationController.java
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/controller/NotificationController.java
@@ -10,22 +10,32 @@ import io.lettuce.core.dynamic.annotation.Param;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/notifications")
 @RequiredArgsConstructor
 public class NotificationController {
     private final NotificationService notificationService;
+    private final String BASE_URL = "localhost:8080.com";
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public ResponseMultiplePaging<NotificationDto.SimpleResponse> getNotifications(
                                                                                       @Param("page") Integer page,
                                                                                       @Param("size") Integer size,
                                                                                       @RequestUser UserDto.UserInfo userInfo) {
         PageRequest pageRequest = PageRequestGenerator.of(page, size);
         return notificationService.findNotificationsBy(userInfo.getId(), pageRequest);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.PERMANENT_REDIRECT)
+    public void getNotificationUri(@PathVariable Long id, HttpServletResponse response) {
+        String redirect_uri = notificationService.findNotificationUriBy(id);
+        response.setHeader("Location",BASE_URL + redirect_uri);
     }
 }
