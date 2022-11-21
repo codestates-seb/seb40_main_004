9bf855d6a44627e09ddd53f9fa0de152c122f1e6
yangddoddi <pud1251@gmail.com>
2022-11-21 23:56:21 +0900

    feat : 유저 알림 반환 레파지토리 구현

--- server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/repository/notification/NotificationQueryRepository.java
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/repository/notification/NotificationQueryRepository.java
@@ -1,10 +1,13 @@
-package com.morakmorak.morak_back_end.repository;
+package com.morakmorak.morak_back_end.repository.notification;
 
 import com.morakmorak.morak_back_end.dto.NotificationDto;
 import com.morakmorak.morak_back_end.dto.QNotificationDto_SimpleResponse;
 import com.morakmorak.morak_back_end.entity.QNotification;
+import com.querydsl.core.types.dsl.EntityPathBase;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
@@ -17,10 +20,10 @@ import static com.morakmorak.morak_back_end.entity.QNotification.*;
 public class NotificationQueryRepository {
     private final JPAQueryFactory queryFactory;
 
-    public List<NotificationDto.SimpleResponse> getNotifications(Long userId, Pageable pageable) {
-        return queryFactory.select(new QNotificationDto_SimpleResponse(
-                notification.id, notification.message, notification.isChecked, notification.createdAt
-        ))
+    public Page<NotificationDto.SimpleResponse> getNotifications(Long userId, Pageable pageable) {
+        List<NotificationDto.SimpleResponse> result = queryFactory.select(new QNotificationDto_SimpleResponse(
+                        notification.id, notification.message, notification.isChecked, notification.createdAt
+                ))
                 .where(notification.user.id.eq(userId))
                 .from(notification)
                 .groupBy(notification.id)
@@ -28,5 +31,12 @@ public class NotificationQueryRepository {
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
+
+        Long count = queryFactory.select(notification.count())
+                .from(notification)
+                .where(notification.user.id.eq(userId))
+                .fetchOne();
+
+        return new PageImpl<>(result, pageable, count);
     }
 }
