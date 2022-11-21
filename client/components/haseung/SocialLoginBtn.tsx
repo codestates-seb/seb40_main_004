dc1aab628519bb13d991a3979d0307784ce068b2
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-11-21 12:07:01 +0900

    docs : 주석 추가

--- client/components/haseung/SocialLoginBtn.tsx
+++ client/components/haseung/SocialLoginBtn.tsx
@@ -1,3 +1,10 @@
+/*
+ * 책임 작성자: 정하승
+ * 최초 작성일: 2022-11-14
+ * 최근 수정일: 2022-11-20
+ * 개요: 소셜 로그인 버튼에 대한 컴포넌트입니다.
+ */
+
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
