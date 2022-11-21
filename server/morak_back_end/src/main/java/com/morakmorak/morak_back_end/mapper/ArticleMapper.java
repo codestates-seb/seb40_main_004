06e989d7ce878d4538eb9883554a9705056b8566
시온백 <tldhs05222@naver.com>
2022-11-22 02:35:04 +0900

    feat : 신고 5번 이상 당한 게시글 상세조회 매퍼 로직 구현 완료

--- server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/mapper/ArticleMapper.java
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/mapper/ArticleMapper.java
@@ -66,6 +66,28 @@ public interface ArticleMapper {
     );
 
 
+    @Mapping(source = "report", target = "title")
+    @Mapping(source = "report", target = "content")
+    @Mapping(source = "article.id", target = "articleId")
+    @Mapping(source = "article.category.name", target = "category")
+    @Mapping(source = "article.user.id", target = "userInfo.userId")
+    @Mapping(source = "article.user.nickname", target = "userInfo.nickname")
+    @Mapping(source = "article.user.grade", target = "userInfo.grade")
+    @Mapping(source = "article.user.avatar.id", target = "avatar.avatarId")
+    @Mapping(source = "article.user.avatar.originalFilename", target = "avatar.filename")
+    @Mapping(source = "article.user.avatar.remotePath", target = "avatar.remotePath")
+    @Mapping(source = "tags", target = "tags")
+    @Mapping(source = "comments", target = "comments")
+    ArticleDto.ResponseDetailArticle articleToResponseBlockedArticle(Article article,
+                                                                     Boolean isLiked,
+                                                                     Boolean isBookmarked,
+                                                                     String report,
+                                                                     List<TagDto.SimpleTag> tags,
+                                                                     List<CommentDto.Response> comments,
+                                                                     Integer likes
+
+    );
+
     ArticleDto.ResponseArticleLike makingResponseArticleLikeDto(Long articleId, Long userId, Boolean isLiked, Integer likeCount);
 
 
