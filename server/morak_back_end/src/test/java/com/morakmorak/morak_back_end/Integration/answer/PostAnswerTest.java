32a16c778f0de766ca02b3add9b7e5d7faa3bd48
yangddoddi <pud1251@gmail.com>
2022-11-22 14:06:16 +0900

    test : 답변 작성 시 포린트 증가 및 알림 기능 테스트

--- server/morak_back_end/src/test/java/com/morakmorak/morak_back_end/Integration/answer/PostAnswerTest.java
+++ server/morak_back_end/src/test/java/com/morakmorak/morak_back_end/Integration/answer/PostAnswerTest.java
@@ -11,6 +11,7 @@ import com.morakmorak.morak_back_end.repository.article.ArticleRepository;
 import com.morakmorak.morak_back_end.repository.FileRepository;
 import com.morakmorak.morak_back_end.repository.user.UserRepository;
 import com.morakmorak.morak_back_end.security.util.JwtTokenUtil;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -187,4 +188,54 @@ public class PostAnswerTest {
                 .andExpect(jsonPath("$.pageInfo.sort.sorted").value(true));
     }
 
+    @Test
+    @DisplayName("답변 작성시 작성자의 포인트가 증가한다.")
+    void postAnswer_success_2() throws Exception {
+        //given
+        articleRepository.save(Article.builder()
+                .user(savedUser)
+                .category(new Category(CategoryName.QNA))
+                .articleStatus(ArticleStatus.POSTING)
+                .isClosed(false).build());
+
+        Article validArticle = articleRepository.findByUserId(savedUser.getId()).orElseThrow(() -> new AssertionError());
+
+        File file1 = fileRepository.findFileByLocalPath("1").orElseThrow(() -> new AssertionError());
+        File file2 = fileRepository.findFileByLocalPath("2").orElseThrow(() -> new AssertionError());
+
+        AnswerDto.RequestPostAnswer request =
+                AnswerDto.RequestPostAnswer.builder()
+                        .content("10자이상 유효한 내용의 답변")
+                        .fileIdList(List.of(FileDto.RequestFileWithId.builder().fileId(file1.getId()).build(),
+                                FileDto.RequestFileWithId.builder().fileId(file2.getId()).build()))
+                        .build();
+
+        String json = objectMapper.writeValueAsString(request);
+
+        User user = User.builder().nickname("nickname").grade(Grade.BRONZE).build();
+
+        Integer beforePoint = user.getPoint();
+
+        Article article = Article.builder().title("테스트 타이틀입니다.")
+                .content("콘탠트입니다. 질문을 많이 올려주세요.")
+                .category(new Category(CategoryName.QNA))
+                .user(user)
+                .files(List.of(file1, file2))
+                .build();
+
+        em.persist(article);
+        String accessToken = jwtTokenUtil.createAccessToken(EMAIL1, user.getId(), ROLE_USER_LIST);
+
+        //when
+        ResultActions perform = mockMvc.perform(
+                post("/articles/{article-id}/answers", article.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+                        .header(JWT_HEADER, accessToken)
+        );
+        Integer afterPoint = user.getPoint();
+
+        //then
+        Assertions.assertThat(beforePoint < afterPoint).isTrue();
+    }
 }
