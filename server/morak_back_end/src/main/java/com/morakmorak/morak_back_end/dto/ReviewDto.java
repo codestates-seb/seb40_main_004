1d5917577b90bbb3f003ef0195b49735bcaa2f0a
Tldkt <spooning.Tldkt@gmail.com>
2022-11-24 23:12:01 +0900

    feat : ReviewDto 추가

--- server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/dto/ReviewDto.java
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/dto/ReviewDto.java
@@ -1,10 +1,15 @@
 package com.morakmorak.morak_back_end.dto;
 
+import com.morakmorak.morak_back_end.entity.Review;
 import com.morakmorak.morak_back_end.entity.enums.Grade;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.*;
 
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class ReviewDto {
     @Builder
@@ -42,6 +47,74 @@ public class ReviewDto {
             this.content = content;
             this.createdAt = createdAt;
             this.userInfo = userInfo;
+        }
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class RequestPostReview {
+        @Size(min = 15)
+        private String content;
+        private List<BadgeDto.SimpleBadge> badges = new ArrayList<>();
+        private Optional<Integer> point;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class ResponseDetailReview {
+
+        private Long reviewId;
+        private Long articleId;
+        private Long answerId;
+        private String content;
+        private Long senderId;
+        private String senderNickname;
+        private Integer remainingPoint;
+        private Long receiverId;
+        private String receiverNickname;
+        private LocalDateTime createdAt;
+        private List<BadgeDto.SimpleBadge> badges = new ArrayList<>();
+        public static ResponseDetailReview of(Review review) {
+
+            ResponseDetailReviewBuilder responseDetailReview = ResponseDetailReview.builder();
+            responseDetailReview.content(review.getContent());
+            responseDetailReview.remainingPoint(review.getSender().getPoint());
+            responseDetailReview.articleId(review.getArticle().getId());
+            responseDetailReview.answerId(review.getAnswer().getId());
+            responseDetailReview.senderId(review.getSender().getId());
+            responseDetailReview.senderNickname(review.getSender().getNickname());
+            responseDetailReview.receiverId(review.getReceiver().getId());
+            responseDetailReview.receiverNickname(review.getReceiver().getNickname());
+            responseDetailReview.reviewId(review.getArticle().getId());
+            responseDetailReview.createdAt(review.getCreatedAt());
+            responseDetailReview.badges(BadgeDto.SimpleBadge.badgeDtoListFrom(review));
+            return responseDetailReview.build();
+
+        }
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class ResponseSimpleReview {
+        private Integer senderRemainingPoint;
+        private Long reviewId;
+        private Long senderId;
+        private Long receiverId;
+        private LocalDateTime createdAt;
+        public static ResponseSimpleReview of(Review review) {
+            return ResponseSimpleReview.builder()
+                    .senderId(review.getSender().getId())
+                    .senderRemainingPoint(review.getSender().getPoint())
+                    .reviewId(review.getId())
+                    .receiverId(review.getReceiver().getId())
+                    .createdAt(review.getCreatedAt())
+                    .build();
         }
     }
 }
