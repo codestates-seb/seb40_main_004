54ba45aec4ab4cd98cc9963a540303ab4c4dfc2d
yangddoddi <pud1251@gmail.com>
2022-11-29 13:26:20 +0900

    refactor : run() 내부의 구현은 별도 메서드로 추출하고 행위만 남겨 가독성 향상, if문도 이해에 시간이 필요한 부분들은 모두 함수화

--- server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/crawler/JobKoreaCrawler.java
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/crawler/JobKoreaCrawler.java
@@ -14,100 +14,114 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static com.morakmorak.morak_back_end.crawler.JobKoreaConstant.*;
+
 @Slf4j
 @RequiredArgsConstructor
 public class JobKoreaCrawler implements Crawler {
-    private final String BASE_URL = "https://www.jobkorea.co.kr/starter/calendar";
-    private final String MONTHLY_DATE = ".lyDate";
-    private final String JOB_FILTER_DEVELOPER_CHECKBOX = "label[for=lb_Jobtype_10016]";
-    private final String JOB_FILTER_ACCEPT_BTN = ".btnFilterSc";
-    private final String JOB_CALENDAR_DAY_DIV_MORE_BTN = ".moreNum";
-    private final String JOB_STATE = ".coLink.devBoothItem strong";
-    private final String JOB_NAME = ".coLink.devBoothItem span";
-    private final String JOB_CAREER_REQUIREMENT = ".sDesc strong";
-    private final String JOB_RECRUITMENT_PERIOD = ".side .day";
-    private final String JOB_HREF = ".coLink.devBoothItem";
-    private final String NEXT_DAY_BTN = ".dateNext";
-    private final String PREV_MONTH_BTN = ".calMonthBtn .prev";
-
-    private final int RANGE = 140;
+    private final int CRAWLING_RANGE = 140;
 
     private final WebDriver webDriver;
     private final WebDriverWait webDriverWait;
     private final JobRepository jobRepository;
 
     @Override
-    public void process() {
-        Set<String> companyNameSet = new HashSet<>();
-        ArrayList<Job> jobs = new ArrayList<>();
-
+    public void run() {
         webDriver.get(BASE_URL);
-        WebElement filterCheckBox = selectElementAndWaitFindByCssSelector(JOB_FILTER_DEVELOPER_CHECKBOX, webDriver, webDriverWait);
-        WebElement filterAcceptBtn = selectElementAndWaitFindByCssSelector(JOB_FILTER_ACCEPT_BTN, webDriver, webDriverWait);
-        setDeveloperFilter(filterCheckBox, filterAcceptBtn);
 
-        WebElement prevMonthBtn = selectElementAndWaitFindByCssSelector(PREV_MONTH_BTN, webDriver, webDriverWait);
-        prevMonthBtn.click();
-        List<WebElement> moreButtons = selectElementsAndWaitFindByCssSelector(JOB_CALENDAR_DAY_DIV_MORE_BTN, webDriver, webDriverWait);
-        moreButtons.get(0).click();
+        setJobSearchFilterDeveloperOnly();
+        moveLastMonthTable();
+        onClickModalBtn();
 
-        for (int i = 0; i < RANGE; i++) {
-            log.info("{}번 테이블 크롤링 시작", i+1);
-
-            try {
-                selectElementAndWaitFindByCssSelector(JOB_STATE, webDriver, webDriverWait);
-            } catch (Exception exception) {
-                log.info("{}번 테이블 예외 발생",i,exception);
-                moveNextPage();
-                continue;
-            }
-            WebElement monthlyDate = selectElementAndWaitFindByCssSelector(MONTHLY_DATE, webDriver, webDriverWait);
-            List<WebElement> states = selectElementsAndWaitFindByCssSelector(JOB_STATE, webDriver, webDriverWait);
-            List<WebElement> urls = selectElementsAndWaitFindByCssSelector(JOB_HREF, webDriver, webDriverWait);
-            List<WebElement> names = selectElementsAndWaitFindByCssSelector(JOB_NAME, webDriver, webDriverWait);
-            List<WebElement> careerRequirements = selectElementsAndWaitFindByCssSelector(JOB_CAREER_REQUIREMENT, webDriver, webDriverWait);
-            List<WebElement> periods = selectElementsAndWaitFindByCssSelector(JOB_RECRUITMENT_PERIOD, webDriver, webDriverWait);
-
-
-            for (int j = 0; j < states.size(); j++) {
-                String year = monthlyDate.getText().split("\\.")[0] + "-";
-                String state = states.get(j).getText();
-                String url = urls.get(j).getAttribute("href");
-                String name = names.get(j).getText().split("\n")[1];
-                String careerRequirement = careerRequirements.get(j).getText();
-                String period = periods.get(j).getText().replace("/", "-");
-
-                if (state.equals("마감") || state.equals("발표") || state.equals("인적성")) continue;
-                if (companyNameSet.contains(name)) continue;
-                if (careerRequirement.equals("신입·경력") || careerRequirement.equals("인턴·신입·경력") ) careerRequirement = "경력무관";
-
-                companyNameSet.add(name);
-                String[] splitPeriod = period.split("~");
-                Date startDay = Date.valueOf(year + splitPeriod[0]);
-                Date endDay = Date.valueOf(year + splitPeriod[1]);
-
-                if (startDay.after(endDay)) {
-                    endDay = Date.valueOf((Integer.parseInt(monthlyDate.getText().split("\\.")[0]) + 1) + "-" + splitPeriod[1]);
-                }
-
-                jobs.add(new Job(name, state, careerRequirement, startDay, endDay, url));
-            }
-
+        Set<String> companyNameSet = new HashSet<>();
+        ArrayList<Job> jobList = new ArrayList<>();
+        for (int i = 0; i < CRAWLING_RANGE; i++) {
+            if (!trySelectElementOnJobTableOf(i)) continue;
+            addJobInJobList(companyNameSet, jobList);
             moveNextPage();
         }
 
-        log.info("크롤링 종료 및 벌크 삽입 시작");
-        jobRepository.saveAllJob(jobs);
+        jobRepository.saveAllJob(jobList);
+    }
+
+    private void onClickModalBtn() {
+        List<WebElement> moreButtons = selectElementsAndWaitFindByCssSelector(JOB_CALENDAR_DAY_DIV_MORE_BTN, webDriver, webDriverWait);
+        moreButtons.get(0).click();
+    }
+
+    private void moveLastMonthTable() {
+        WebElement prevMonthBtn = selectElementAndWaitFindByCssSelector(PREV_MONTH_BTN, webDriver, webDriverWait);
+        prevMonthBtn.click();
+    }
+
+    private void setJobSearchFilterDeveloperOnly() {
+        WebElement filterCheckBox = selectElementAndWaitFindByCssSelector(JOB_FILTER_DEVELOPER_CHECKBOX, webDriver, webDriverWait);
+        WebElement filterAcceptBtn = selectElementAndWaitFindByCssSelector(JOB_FILTER_ACCEPT_BTN, webDriver, webDriverWait);
+        filterCheckBox.click();
+        filterAcceptBtn.click();
+    }
+
+
+    private void addJobInJobList(Set<String> companyNameSet, ArrayList<Job> jobList) {
+        WebElement monthlyDate = selectElementAndWaitFindByCssSelector(MONTHLY_DATE, webDriver, webDriverWait);
+        List<WebElement> states = selectElementsAndWaitFindByCssSelector(JOB_STATE, webDriver, webDriverWait);
+        List<WebElement> urls = selectElementsAndWaitFindByCssSelector(JOB_HREF, webDriver, webDriverWait);
+        List<WebElement> names = selectElementsAndWaitFindByCssSelector(JOB_NAME, webDriver, webDriverWait);
+        List<WebElement> careerRequirements = selectElementsAndWaitFindByCssSelector(JOB_CAREER_REQUIREMENT, webDriver, webDriverWait);
+        List<WebElement> periods = selectElementsAndWaitFindByCssSelector(JOB_RECRUITMENT_PERIOD, webDriver, webDriverWait);
+
+
+        for (int j = 0; j < states.size(); j++) {
+            String year = monthlyDate.getText().split("\\.")[0] + "-";
+            String state = states.get(j).getText();
+            String url = urls.get(j).getAttribute("href");
+            String name = names.get(j).getText().split("\n")[1];
+            String careerRequirement = careerRequirements.get(j).getText();
+            String period = periods.get(j).getText().replace("/", "-");
+
+            if (recruitmentIsNotStartingState(state)) continue;
+            if (isDuplicateCompanyName(companyNameSet, name)) continue;
+            if (careerRequirement.equals("신입·경력") || careerRequirement.equals("인턴·신입·경력") ) careerRequirement = "경력무관";
+
+            companyNameSet.add(name);
+            String[] splitPeriod = period.split("~");
+            Date startDay = Date.valueOf(year + splitPeriod[0]);
+            Date endDay = Date.valueOf(year + splitPeriod[1]);
+
+            if (startDay.after(endDay)) {
+                endDay = changeYearToNextYear(monthlyDate, splitPeriod);
+            }
+
+            jobList.add(new Job(name, state, careerRequirement, startDay, endDay, url));
+        }
+    }
+
+    private Date changeYearToNextYear(WebElement monthlyDate, String[] splitPeriod) {
+        return Date.valueOf((Integer.parseInt(monthlyDate.getText().split("\\.")[0]) + 1) + "-" + splitPeriod[1]);
+    }
+
+    private boolean isDuplicateCompanyName(Set<String> companyNameSet, String name) {
+        return companyNameSet.contains(name);
+    }
+
+    private boolean recruitmentIsNotStartingState(String state) {
+        return state.equals("마감") || state.equals("발표") || state.equals("인적성");
+    }
+
+    private boolean trySelectElementOnJobTableOf(int i) {
+        try {
+            selectElementAndWaitFindByCssSelector(JOB_STATE, webDriver, webDriverWait);
+        } catch (Exception exception) {
+            log.info("{}번 테이블 예외 발생", i,exception);
+            moveNextPage();
+            return false;
+        }
+        return true;
     }
 
     private void moveNextPage() {
         WebElement nextDayBtn = selectElementAndWaitFindByCssSelector(NEXT_DAY_BTN, webDriver, webDriverWait);
         nextDayBtn.click();
-    }
-
-    private void setDeveloperFilter(WebElement filterCheckBox, WebElement filterAcceptBtn) {
-        filterCheckBox.click();
-        filterAcceptBtn.click();
     }
 
     @Override
