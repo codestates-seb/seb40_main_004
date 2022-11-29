ba7be567e90c2ecb4357c09161f597c33171f530
Tldkt <spooning.Tldkt@gmail.com>
2022-11-29 23:46:36 +0900

    test : article 삭제 시 예외처리 restdocs 작성

--- server/morak_back_end/src/test/java/com/morakmorak/morak_back_end/controller/article_controller/ArticleControllerTest.java
+++ server/morak_back_end/src/test/java/com/morakmorak/morak_back_end/controller/article_controller/ArticleControllerTest.java
@@ -1,12 +1,14 @@
 package com.morakmorak.morak_back_end.controller.article_controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.morakmorak.morak_back_end.config.SecurityTestConfig;
 import com.morakmorak.morak_back_end.controller.ArticleController;
 import com.morakmorak.morak_back_end.controller.ExceptionController;
-import com.morakmorak.morak_back_end.dto.*;
-import com.morakmorak.morak_back_end.entity.*;
+import com.morakmorak.morak_back_end.dto.ArticleDto;
+import com.morakmorak.morak_back_end.dto.UserDto;
+import com.morakmorak.morak_back_end.entity.Article;
+import com.morakmorak.morak_back_end.entity.Category;
 import com.morakmorak.morak_back_end.entity.enums.ReportReason;
-import com.morakmorak.morak_back_end.entity.enums.TagName;
 import com.morakmorak.morak_back_end.exception.BusinessLogicException;
 import com.morakmorak.morak_back_end.exception.ErrorCode;
 import com.morakmorak.morak_back_end.mapper.ArticleMapper;
@@ -16,7 +18,6 @@ import com.morakmorak.morak_back_end.mapper.TagMapper;
 import com.morakmorak.morak_back_end.repository.article.ArticleRepository;
 import com.morakmorak.morak_back_end.security.resolver.JwtArgumentResolver;
 import com.morakmorak.morak_back_end.service.ArticleService;
-import com.morakmorak.morak_back_end.config.SecurityTestConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,20 +35,21 @@ import org.springframework.test.web.servlet.ResultActions;
 import java.util.List;
 
 import static com.morakmorak.morak_back_end.util.ArticleTestConstants.*;
-import static com.morakmorak.morak_back_end.util.SecurityTestConstants.*;
+import static com.morakmorak.morak_back_end.util.SecurityTestConstants.ACCESS_TOKEN;
+import static com.morakmorak.morak_back_end.util.SecurityTestConstants.JWT_HEADER;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.anyLong;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @WebMvcTest({ArticleController.class, ExceptionController.class})
@@ -227,188 +229,9 @@ class ArticleControllerTest {
                         )));
     }
 
-    @Test
-    @DisplayName("게시글을 수정할때 성공 테스트")
-    public void articleUpdate_suc() throws Exception {
-        //given
-        ArticleDto.RequestUpdateArticle request = ArticleDto.RequestUpdateArticle.builder()
-                .title("타이틀입니다. 잘부탁드립니다. 부탁드립니다.").content("콘텐트입니다. 잘부탁드립니다.")
-                .fileId(List.of(FileDto.RequestFileWithId.builder().fileId(1L).build()))
-                .tags(List.of(TagDto.SimpleTag.builder().name(TagName.JAVA).tagId(1L).build()))
-                .thumbnail(1L)
-                .build();
-        ArticleDto.ResponseSimpleArticle response = ArticleDto.ResponseSimpleArticle.builder().articleId(1L).build();
 
-        given(articleMapper.requestUpdateArticleToEntity(request, 1L))
-                .willReturn(Article.builder().build());
-        given(tagMapper.requestTagWithIdAndNameToTagDto(request))
-                .willReturn(List.of(TagDto.SimpleTag.builder().build()));
-        given(fileMapper.RequestFileWithIdToFile(request))
-                .willReturn(List.of(FileDto.RequestFileWithId.builder().build()));
-        given(articleService.update(any(), any(), any(), any()))
-                .willReturn(response);
 
-        String content = objectMapper.writeValueAsString(request);
-        //when
-        ResultActions perform = mockMvc.perform(
-                patch("/articles/{article-id}",1)
-                        .header(JWT_HEADER, ACCESS_TOKEN)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(content)
-        );
-        //then
-        perform.andExpect(status().isOk())
-                .andExpect(jsonPath("$.articleId").value(1L))
-                .andDo(document(
-                        "게시글_수정에_성공할_경우_성공_200",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        pathParameters(
-                                parameterWithName("article-id").description("게시글의 아이디 입니다.")
-                        ),
-                        requestHeaders(
-                                headerWithName(JWT_HEADER).description("access token")
-                        ),
-                        requestFields(
-                                List.of(
-                                        fieldWithPath("title").type(JsonFieldType.STRING).description("글의 제목입니다."),
-                                        fieldWithPath("content").type(JsonFieldType.STRING).description("글의 본문입니다."),
-                                        fieldWithPath("tags[].tagId").type(JsonFieldType.NUMBER).description("태그 아이디 입니다."),
-                                        fieldWithPath("tags[].name").type(JsonFieldType.STRING).description("태그 이름입니다."),
-                                        fieldWithPath("fileId[].fileId").type(JsonFieldType.NUMBER).description("파일 아이디 입니다."),
-                                        fieldWithPath("thumbnail").type(JsonFieldType.NUMBER).description("글의 썸네일 아이디 입니다.")
-                                )
-                        ),
-                        responseFields(
-                                List.of(
-                                        fieldWithPath("articleId").type(JsonFieldType.NUMBER).description("글의 아이디 입니다.")
-                                )
-                        )
-                ));
-    }
 
-    @Test
-    @DisplayName("게시글을 수정할때 유효성 검증에 실패한 경우 400에러를 던진다. ")
-    public void articleUpdate_fail1() throws Exception {
-        //given
-        ArticleDto.RequestUpdateArticle requestUploadArticle = ArticleDto.RequestUpdateArticle.builder()
-                .title("타이틀").content("콘텐트")
-                .fileId(List.of(FileDto.RequestFileWithId.builder().fileId(1L).build()))
-                .tags(List.of(TagDto.SimpleTag.builder().name(TagName.JAVA).tagId(1L).build()))
-                .thumbnail(1L)
-                .build();
-        String content = objectMapper.writeValueAsString(requestUploadArticle);
-
-        //when
-        ResultActions perform =
-                mockMvc.perform(
-                        patch("/articles/{article-id}",1)
-                                .header(JWT_HEADER, ACCESS_TOKEN)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(content)
-                );
-        perform.andExpect(status().isBadRequest())
-                .andDo(document(
-                        "게시글을_수정시_유효성_검증에_실패_400",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        pathParameters(
-                                parameterWithName("article-id").description("게시글의 아이디 입니다.")
-                        ),
-                        requestHeaders(
-                                headerWithName(JWT_HEADER).description("access token")
-                        ),
-                        requestFields(
-                                List.of(
-                                        fieldWithPath("title").type(JsonFieldType.STRING).description("글의 제목입니다."),
-                                        fieldWithPath("content").type(JsonFieldType.STRING).description("글의 본문입니다."),
-                                        fieldWithPath("tags[].tagId").type(JsonFieldType.NUMBER).description("태그 아이디 입니다."),
-                                        fieldWithPath("tags[].name").type(JsonFieldType.STRING).description("태그 이름입니다."),
-                                        fieldWithPath("fileId[].fileId").type(JsonFieldType.NUMBER).description("파일 아이디 입니다."),
-                                        fieldWithPath("thumbnail").type(JsonFieldType.NUMBER).description("글의 썸네일 아이디 입니다.")
-                                )
-                        )));
-    }
-
-    @Test
-    @DisplayName("게시글을 삭제할때 성공 테스트")
-    public void articleDelete_suc() throws Exception {
-        //given
-        Article article = Article.builder().id(1L).user(User.builder().id(1L).build()).build();
-        UserDto.UserInfo userInfo = UserDto.UserInfo.builder().id(1L).build();
-
-        given(articleService.deleteArticle(any(),any())).willReturn(true);
-
-        //when
-        ResultActions perform = mockMvc.perform(
-                delete("/articles/{article-id}",1)
-                        .header(JWT_HEADER, ACCESS_TOKEN)
-        );
-
-        //then
-
-        perform.andExpect(status().isNoContent())
-                .andDo(document(
-                        "게시글을_삭제할때_본인의_게시글을_삭제할때_성공_204",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        pathParameters(
-                                parameterWithName("article-id").description("게시글의 아이디 입니다.")
-                        ),
-                        requestHeaders(
-                                headerWithName(JWT_HEADER).description("access token")
-                        )
-                ));
-    }
-
-    @Test
-    @DisplayName("게시글작성자가 아닌 유저가 해당 게시글을 삭제시 ")
-    public void deleteArticle_fail1() throws Exception{
-        //given
-        given(articleService.deleteArticle(anyLong(), any(UserDto.UserInfo.class)))
-                .willThrow(new BusinessLogicException(ErrorCode.INVALID_USER));
-        //when
-        ResultActions perform = mockMvc.perform(
-                delete("/articles/{article-id}",1)
-                        .header(JWT_HEADER, ACCESS_TOKEN)
-        );
-        //then
-        perform.andExpect(status().isUnauthorized()) .andDo(document(
-                "게시글_삭제시_타인의_게시글을_삭제하려할떄_403",
-                preprocessRequest(prettyPrint()),
-                preprocessResponse(prettyPrint()),
-                pathParameters(
-                        parameterWithName("article-id").description("게시글의 아이디 입니다.")
-                ),
-                requestHeaders(
-                        headerWithName(JWT_HEADER).description("access token")
-                )));
-     }
-
-    @Test
-    @DisplayName("게시글 삭제시 게시글이 존재하지 않을경우 404 Article_Not_Found 를 던진다. ")
-    public void deleteArticle_fail2() throws Exception{
-        //given
-        given(articleService.deleteArticle(anyLong(), any(UserDto.UserInfo.class)))
-                .willThrow(new BusinessLogicException(ErrorCode.ARTICLE_NOT_FOUND));
-        //when
-        ResultActions perform = mockMvc.perform(
-                delete("/articles/{article-id}",1)
-                        .header(JWT_HEADER, ACCESS_TOKEN)
-        );
-        //then
-        perform.andExpect(status().isNotFound())
-                .andDo(document(
-                        "게시글_삭제시_게시글이_존재하지_않을경우_실패_404",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        pathParameters(
-                                parameterWithName("article-id").description("게시글의 아이디 입니다.")
-                        ),
-                        requestHeaders(
-                                headerWithName(JWT_HEADER).description("access token")
-                        )));
-    }
 
     @Test
     @DisplayName("로그인한 회원이 게시글의 좋아요를 눌렀을때 처음 좋아요를 눌렀을 경우 좋아요 갯수가 1개 증가하고 200 코드를 던진다.")
