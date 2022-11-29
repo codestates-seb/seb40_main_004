127e4e3ed8f6d3adbe780b577675380c74b0c584
HyeonWooGa <yeonwoopark22@gmail.com>
2022-11-29 23:22:17 +0900

    update : 프로필 수정 컴포넌트 생성

--- client/pages/edit-profile/index.tsx
+++ client/pages/edit-profile/index.tsx
@@ -1,10 +1,12 @@
 /*
  * 책임 작성자: 박연우
- * 최초 작성일: 2022-11-14
- * 최근 수정일: 2022-11-20
+ * 최초 작성일: 2022-11-29
+ * 최근 수정일: 2022-11-29
  */
 
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { Footer } from '../../components/common/Footer';
 import { Header } from '../../components/common/Header';
 import { AsideEditProfile } from '../../components/yeonwoo/AsideEditProfile';
@@ -12,6 +14,15 @@ import { EditAvatar } from '../../components/yeonwoo/EditAvatar';
 import { EditProfileComponent } from '../../components/yeonwoo/EditProfile';
 
 const EditProfile: NextPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (!localStorage.getItem('refreshToken')) {
+        alert('로그인이 필요한 페이지입니다');
+        router.push('/');
+      }
+    }
+  }, []);
   return (
     <>
       <Header />
