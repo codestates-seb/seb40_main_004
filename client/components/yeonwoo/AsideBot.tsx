029a3b6d04fc055272d29c17a8825443c620711d
HyeonWooGa <yeonwoopark22@gmail.com>
2022-11-29 23:12:38 +0900

    update : AsideBot api 연결

--- client/components/yeonwoo/AsideBot.tsx
+++ client/components/yeonwoo/AsideBot.tsx
@@ -4,14 +4,25 @@
  * 최근 수정일: 2022-11-19
  */
 
+import { useRecoilValue } from 'recoil';
+import { userDashboardAtom } from '../../atomsYW';
+
 export const AsideBot = () => {
+  const userDashboard = useRecoilValue(userDashboardAtom);
   return (
     <div className="mt-[74px]">
-      <div>
+      <div className="mb-6">
         <span className="text-2xl font-semibold">기술 태그</span>
       </div>
       <div>
-        <div className="mt-6">
+        {userDashboard.reviewBadges.map((badge) => (
+          <div className="mb-4" key={badge.badge_Id}>
+            <button className="bg-main-yellow rounded-full py-[6px] w-32">
+              {badge.name}
+            </button>
+          </div>
+        ))}
+        {/*<div className="mt-6">
           <button className="bg-main-yellow rounded-full py-[6px] w-32">
             React
           </button>
@@ -25,7 +36,7 @@ export const AsideBot = () => {
           <button className="bg-main-yellow rounded-full py-[6px] w-32">
             CSS
           </button>
-        </div>
+        </div>*/}
       </div>
     </div>
   );
