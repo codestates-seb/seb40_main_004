6bd5f6ce5bac8fcb0adf28419a1a2ec4f67b6e96
Tldkt <spooning.Tldkt@gmail.com>
2022-12-01 00:03:08 +0900

    fix : answer 파일 첨부 시 500 에러 npe 수정

--- server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/service/FileService.java
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/service/FileService.java
@@ -7,7 +7,9 @@ import com.morakmorak.morak_back_end.exception.ErrorCode;
 import com.morakmorak.morak_back_end.repository.FileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,6 +18,7 @@ import java.util.stream.Collectors;
 public class FileService {
     private final FileRepository fileRepository;
     public List<File> createFileListFrom(List<FileDto.RequestFileWithId> fileIdList) {
+        if (ObjectUtils.isEmpty(fileIdList)) {return Collections.emptyList();}
         return fileIdList.stream().map(request ->
                         fileRepository.findById(request.getFileId()).orElseThrow(()
                                 -> new BusinessLogicException(ErrorCode.FILE_NOT_FOUND)))
