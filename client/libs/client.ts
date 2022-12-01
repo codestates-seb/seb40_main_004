4a75b92780237fd239715cbca3ca9b2673b612a7
Hyejung Park <hyejj19@naver.com>
2022-12-02 01:14:00 +0900

    feat : axios interceptor 를 활용한 리프레시 토큰 갱신 기능 구현
    * 원인은 알 수 없으나 간헐적으로 갱신 요청시 404가 뜨는 현상이 있습니다.
    * 현재 추측으로는 여러개의 요청이 중첩되어 들어가다보니 그런 것 같은데, 새로고침을 하면 다시 잘 동작하는 것 확인되었습니다.

--- client/libs/client.ts
+++ client/libs/client.ts
@@ -8,14 +8,39 @@ if (typeof window !== 'undefined') {
 export const client = axios.create({
   headers: {
     withCredentials: true,
-    Authorization: `${accessToken}`,
+    Authorization: accessToken,
     'Content-Type': `application/json`,
     'ngrok-skip-browser-warning': '111',
   },
 });
 
+const requestArray: any = [];
+
+client.interceptors.request.use(
+  async (config) => {
+    const accessToken = localStorage.getItem('accessToken');
+    config.headers = {
+      withCredentials: true,
+      Authorization: `${accessToken}`,
+      'Content-Type': `application/json`,
+      'ngrok-skip-browser-warning': '111',
+    };
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  },
+);
+
 client.interceptors.response.use(
   (response) => {
+    if (requestArray.length != 0) {
+      requestArray.forEach(function (x: any, i: any) {
+        if (response.config.url == x.url) {
+          requestArray.splice(i, 1);
+        }
+      });
+    }
     return response;
   },
   async (error) => {
@@ -24,25 +49,32 @@ client.interceptors.response.use(
       response: { status },
     } = error;
     if (status === 401) {
-      if (error.response.data.message === 'TokenExpiredError') {
-        const originalRequest = config;
-        const refreshToken = localStorage.getItem('refreshToken');
-
-        const { data } = await client.put(`/api/auth/token`, {
+      const originalRequest = config;
+      const { data } = await axios.put(
+        `/api/auth/token`,
+        {},
+        {
           headers: {
-            RefreshToken: refreshToken,
+            RefreshToken: localStorage.getItem('refreshToken'),
+            'ngrok-skip-browser-warning': '111',
           },
+        },
+      );
+      const { accessToken: newAccessToken, refreshToken: newRefreshToken } =
+        data;
+      localStorage.setItem('accessToken', newAccessToken);
+      localStorage.setItem('refreshToken', newRefreshToken);
+      if (requestArray.length !== 0) {
+        requestArray.forEach((x: any) => {
+          try {
+            x.headers.Authorization = newAccessToken;
+            client.defaults.headers.common['Authorization'] = newAccessToken;
+          } catch (err) {
+            console.log(err);
+          }
         });
-        const { accessToken: newAccessToken, refreshToken: newRefreshToken } =
-          data;
-        localStorage.setItem('accessToken', newAccessToken);
-        localStorage.setItem('refreshToken', newRefreshToken);
-
-        axios.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
-        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-
-        return axios(originalRequest);
       }
+      return client(originalRequest);
     }
     return Promise.reject(error);
   },
