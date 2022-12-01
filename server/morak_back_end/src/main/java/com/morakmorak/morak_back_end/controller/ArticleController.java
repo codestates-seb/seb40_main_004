fc7a9ea24c5710739620ea0f7add827c11059158
시온백 <tldhs05222@naver.com>
2022-12-01 23:23:48 +0900

    refactor : ArticleController 가독성 향상 및 빠져있던 제네릭 추가

--- server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/controller/ArticleController.java
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/controller/ArticleController.java
@@ -12,7 +12,6 @@ import io.lettuce.core.dynamic.annotation.Param;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,91 +33,78 @@ public class ArticleController {
 
 
     @PostMapping
-    public ResponseEntity uploadArticle(@Valid @RequestBody ArticleDto.RequestUploadArticle requestUploadArticle,
-                                        @RequestUser UserDto.UserInfo userInfo) {
-        log.info("UPLOAD_ARTICLE_INPUT_{}", requestUploadArticle);
+    @ResponseStatus(HttpStatus.CREATED)
+    public ArticleDto.ResponseSimpleArticle uploadArticle(@Valid @RequestBody ArticleDto.RequestUploadArticle requestUploadArticle,
+                                                          @RequestUser UserDto.UserInfo userInfo) {
+
         Article article = articleMapper.requestUploadArticleToEntity(requestUploadArticle);
         List<TagDto.SimpleTag> tags = tagMapper.requestTagWithIdAndNameToTagDto(requestUploadArticle);
         List<FileDto.RequestFileWithId> files = fileMapper.RequestFileWithIdToFile(requestUploadArticle);
         Category category = categoryMapper.RequestUploadArticleToCategory(requestUploadArticle);
-        ArticleDto.ResponseSimpleArticle upload = articleService.upload(article, userInfo, tags, files, category);
-        log.info("UPLOAD_ARTICLE_OUTPUT_{}", upload);
-        return new ResponseEntity<>(upload, HttpStatus.CREATED);
+
+        return articleService.upload(article, userInfo, tags, files, category);
+
+
     }
 
     @PatchMapping("/{article-id}")
-    public ResponseEntity updateArticle(@Valid @RequestBody ArticleDto.RequestUpdateArticle requestUpdateArticle,
-                                        @RequestUser UserDto.UserInfo userInfo,
-                                        @PathVariable("article-id") Long articleId) {
-        log.info("UPDATE_ARTICLE_INPUT_{}", requestUpdateArticle);
-        log.info("UPDATE_ARTICLE_USERINFO_INPUT_{}", userInfo);
+    @ResponseStatus(HttpStatus.OK)
+    public ArticleDto.ResponseSimpleArticle updateArticle(@Valid @RequestBody ArticleDto.RequestUpdateArticle requestUpdateArticle,
+                                                          @RequestUser UserDto.UserInfo userInfo,
+                                                          @PathVariable("article-id") Long articleId) {
 
         Article article = articleMapper.requestUpdateArticleToEntity(requestUpdateArticle, articleId);
         List<TagDto.SimpleTag> tags = tagMapper.requestTagWithIdAndNameToTagDto(requestUpdateArticle);
         List<FileDto.RequestFileWithId> files = fileMapper.RequestFileWithIdToFile(requestUpdateArticle);
-        ArticleDto.ResponseSimpleArticle update = articleService.update(article, userInfo, tags, files);
 
-        log.info("UPDATE_ARTICLE_OUTPUT_{}", update);
-        return new ResponseEntity<>(update, HttpStatus.OK);
+        return articleService.update(article, userInfo, tags, files);
     }
 
     @DeleteMapping("/{article-id}")
-    public ResponseEntity deleteArticle(@PathVariable("article-id") Long articleId,
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteArticle(@PathVariable("article-id") Long articleId,
                                         @RequestUser UserDto.UserInfo userInfo) {
-        log.info("DELETE_ARTICLE_INPUT_{}", articleId);
-        log.info("DELETE_ARTICLE_USERINFO_INPUT_{}", userInfo);
+
         articleService.deleteArticle(articleId, userInfo);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping
-    public ResponseEntity searchArticle(@Param("category") String category,
-                                        @Param("keyword") String keyword,
-                                        @Param("target") String target,
-                                        @Param("sort") String sort,
-                                        @RequestParam("page") Integer page,
-                                        @RequestParam("size") Integer size) {
-        log.info("SEARCHARTICLE_INPUT_PARAM_{}",category);
-        log.info("SEARCHARTICLE_INPUT_PARAM_{}",keyword);
-        log.info("SEARCHARTICLE_INPUT_PARAM_{}",target);
-        log.info("SEARCHARTICLE_INPUT_PARAM_{}",sort);
-        log.info("SEARCHARTICLE_INPUT_PARAM_{}",page);
-        log.info("SEARCHARTICLE_INPUT_PARAM_{}",size);
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseMultiplePaging<ArticleDto.ResponseListTypeArticle> searchArticle(@Param("category") String category,
+                                                                                    @Param("keyword") String keyword,
+                                                                                    @Param("target") String target,
+                                                                                    @Param("sort") String sort,
+                                                                                    @RequestParam("page") Integer page,
+                                                                                    @RequestParam("size") Integer size) {
 
-        ResponseMultiplePaging responseMultiplePaging = articleService.searchArticleAsPaging(category, keyword, target, sort, page, size);
+        return articleService.searchArticleAsPaging(category, keyword, target, sort, page, size);
 
-        log.info("SEARCHARTICLE_OUTPUT_{}",responseMultiplePaging);
-        return new ResponseEntity(responseMultiplePaging, HttpStatus.OK);
     }
 
     @GetMapping("/{article-id}")
-    public ResponseEntity findDetailArticle(@RequestUser UserDto.UserInfo userInfo,
+    @ResponseStatus(HttpStatus.OK)
+    public ArticleDto.ResponseDetailArticle findDetailArticle(@RequestUser UserDto.UserInfo userInfo,
                                           @PathVariable("article-id") Long articleId) {
-        log.info("FIND_DETAIL_ARTICLE_INPUT_{}", articleId);
-        log.info("FIND_DETAIL_ARTICLE_USERINFO_INPUT_{}", userInfo);
-        ArticleDto.ResponseDetailArticle result = articleService.findDetailArticle(articleId, userInfo);
-        log.info("FIND_DETAIL_ARTICLE_OUTPUT_{}", result);
-        return new ResponseEntity(result, HttpStatus.OK);
+
+        return articleService.findDetailArticle(articleId, userInfo);
     }
 
     @PostMapping("/{article-id}/likes")
-    public ResponseEntity pressLikeButton(@RequestUser UserDto.UserInfo userInfo,
+    @ResponseStatus(HttpStatus.OK)
+    public ArticleDto.ResponseArticleLike pressLikeButton(@RequestUser UserDto.UserInfo userInfo,
                                           @PathVariable("article-id") Long articleId) {
-        log.info("PRESS_LIKE_BUTTON_USERINFO_INPUT_{}",userInfo);
-        log.info("PRESS_LIKE_BUTTON_INPUT_{}",articleId);
-        ArticleDto.ResponseArticleLike responseArticleLike = articleService.pressLikeButton(articleId, userInfo);
-        log.info("PRESS_LIKE_BUTTON_OUTPUT_{}", responseArticleLike);
-        return new ResponseEntity(responseArticleLike, HttpStatus.OK);
+
+        return articleService.pressLikeButton(articleId, userInfo);
     }
 
     @PostMapping("/{article-id}/reports")
-    public ResponseEntity reportArticle(@PathVariable("article-id") Long articleId,
-                                        @RequestBody ArticleDto.RequestReportArticle reportArticle,
-                                        @RequestUser UserDto.UserInfo userInfo) {
-        ArticleDto.ResponseReportArticle responseReportArticle =
-                articleService.reportArticle(articleId, userInfo, articleMapper.requestReportArticleToReport(reportArticle));
-
-        return new ResponseEntity(responseReportArticle, HttpStatus.CREATED);
+    @ResponseStatus(HttpStatus.CREATED)
+    public ArticleDto.ResponseReportArticle reportArticle(@PathVariable("article-id") Long articleId,
+                                                          @RequestBody ArticleDto.RequestReportArticle reportArticle,
+                                                          @RequestUser UserDto.UserInfo userInfo) {
+        return articleService.reportArticle(articleId,
+                userInfo,
+                articleMapper.requestReportArticleToReport(reportArticle));
     }
 
 }
