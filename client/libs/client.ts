306ec88fceee97b6b397a33b813df621b40a5444
Hyejung Park <hyejj19@naver.com>
2022-12-01 15:33:42 +0900

    feat : 리프레시 토큰 갱신로직
    * 아직 테스트 전이라 동작 여부는 파악되지 않았습니다. 코드리뷰를 위해 먼저 올립니다.

--- client/libs/client.ts
+++ client/libs/client.ts
@@ -13,3 +13,37 @@ export const client = axios.create({
     'ngrok-skip-browser-warning': '111',
   },
 });
+
+client.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const {
+      config,
+      response: { status },
+    } = error;
+    if (status === 401) {
+      if (error.response.data.message === 'TokenExpiredError') {
+        const originalRequest = config;
+        const refreshToken = localStorage.getItem('refreshToken');
+
+        const { data } = await client.put(`/api/auth/token`, {
+          headers: {
+            RefreshToken: refreshToken,
+          },
+        });
+        const { accessToken: newAccessToken, refreshToken: newRefreshToken } =
+          data;
+        localStorage.setItem('accessToken', newAccessToken);
+        localStorage.setItem('refreshToken', newRefreshToken);
+
+        axios.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return axios(originalRequest);
+      }
+    }
+    return Promise.reject(error);
+  },
+);
