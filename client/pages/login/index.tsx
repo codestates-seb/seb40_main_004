0d1edb29caa8d575f85e279c1cbcb08a00a6cfe9
Hyejung Park <hyejj19@naver.com>
2022-12-01 14:35:29 +0900

    style : 로그인, 회원가입 form UI 업데이트

--- client/pages/login/index.tsx
+++ client/pages/login/index.tsx
@@ -1,21 +1,27 @@
 /*
  * 책임 작성자: 정하승
  * 최초 작성일: 2022-11-14
- * 최근 수정일: 2022-11-15
+ * 최근 수정일: 2022-12-01 (박혜정)
  * 개요: 로그인에 대한 form을 표시합니다.
  */
 
 import { LoginForm } from '../../components/haseung/LoginForm';
+import { Header } from '../../components/common/Header';
+import { Footer } from '../../components/common/Footer';
 
 const Login = () => {
   return (
-    <main className="flex justify-center text-lg ">
-      <article className="text-center mt-10">
-        <h3 className="font-bold">로그인</h3>
-        <h3 className="mt-5">다시 오셨군요! 환영합니다.</h3>
-        <LoginForm />
-      </article>
-    </main>
+    <div className="h-screen">
+      <Header />
+      <main className="flex flex-col justify-center items-center h-[79vh] bg-white">
+        <article className="text-center">
+          <h3 className="font-bold text-2xl">로그인</h3>
+          <h3 className="mt-4 font-semibold">다시 오셨군요! 환영합니다.</h3>
+          <LoginForm />
+        </article>
+      </main>
+      <Footer />
+    </div>
   );
 };
 
--- client/pages/signup-email/index.tsx
+++ client/pages/signup-email/index.tsx
@@ -1,7 +1,7 @@
 /*
  * 책임 작성자: 정하승
  * 최초 작성일: 2022-11-15
- * 최근 수정일: 2022-11-22
+ * 최근 수정일: 2022-12-01(박혜정)
  */
 
 import axios from 'axios';
@@ -11,6 +11,8 @@ import { useRecoilState, useRecoilValue } from 'recoil';
 import { userAuthKey, userEmailAtom } from '../../atomsHS';
 import { AuthenticationTimer } from '../../components/haseung/AuthenticationTimer';
 import { Intro } from '../../components/haseung/Intro';
+import { Header } from '../../components/common/Header';
+import { Footer } from '../../components/common/Footer';
 
 type VerificationNumber = {
   authKey: string;
@@ -33,29 +35,35 @@ const AuthenticateNumber = () => {
     router.push('/signup-status');
   };
   return (
-    <main className="flex justify-center text-lg">
-      <article className="text-center mt-10">
-        <Intro />
-        <section className="text-background-gray font-semibold flex justify-center items-center bg-main-gray w-[400px] h-20 mt-10 rounded-full">
-          이메일로 인증번호가 전송되었습니다!
-        </section>
-        <section className="flex justify-between mt-3">
-          <span>인증번호</span>
-          <AuthenticationTimer />
-        </section>
-        <form onSubmit={handleSubmit(onValid)}>
-          <input
-            {...register('authKey', { required: true })}
-            type="text"
-            placeholder="1234"
-            className="placeholder:px-3 flex justify-center items-center w-[400px] h-12 mt-10 rounded-full"
-          />
-          <button className="bg-main-yellow font-semibold hover:bg-main-orange placeholder:px-3 flex justify-center items-center w-[400px] h-12 mt-10 rounded-full">
-            인증하기
-          </button>
-        </form>
-      </article>
-    </main>
+    <div className="h-screen">
+      <Header />
+      <main className="flex flex-col justify-center items-center h-[79vh] bg-white">
+        <article className="text-center mt-10 flex flex-col justify-center items-center w-full px-12">
+          <Intro />
+          <section className="text-background-gray font-semibold flex justify-center items-center bg-main-gray w-full h-20 mt-10 rounded-[20px]">
+            이메일로 인증번호가 전송되었습니다!
+          </section>
+          <section className="flex justify-between w-full mt-10">
+            <span className="font-bold">인증번호</span>
+            <AuthenticationTimer />
+          </section>
+          <form onSubmit={handleSubmit(onValid)} className="w-full">
+            <input
+              {...register('authKey', { required: true })}
+              type="text"
+              placeholder="1234"
+              className="rounded-full w-full h-10 pl-4
+              border
+              my-5"
+            />
+            <button className="bg-main-yellow py-3 w-full rounded-[20px] font-bold mb-5">
+              인증하기
+            </button>
+          </form>
+        </article>
+      </main>
+      <Footer />
+    </div>
   );
 };
 
