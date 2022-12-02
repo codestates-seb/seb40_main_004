f06865dca667c55341c891995519df4d46f4a9fc
yangddoddi <pud1251@gmail.com>
2022-12-02 11:07:29 +0900

    update : 메서드 실행 시간에 대한 경고 문구가 더 눈에 잘 띄도록 변경

--- server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/interceptor/PerformanceLoggingInterceptor.java
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/interceptor/PerformanceLoggingInterceptor.java
@@ -21,8 +21,14 @@ public class PerformanceLoggingInterceptor extends AbstractMonitoringInterceptor
             logger.info("Method " + name + " execution lasted:" + time + " ms");
             logger.info("Method " + name + " execution ended at:" + new Date());
 
-            if (time > 10) {
-                logger.warn("Method execution longer than 10 ms");
+            if (time > 4000) {
+                logger.warn("☠️☠️☠️☠️☠️ 해당 메서드의 실행 시간이 4000ms를 초과했습니다. ☠️☠️☠️☠️☠️");
+            }else if (time > 3000) {
+                logger.warn("🚨🚨🚨🚨🚨 해당 메서드의 실행 시간이 3000ms를 초과했습니다. 🚨🚨🚨🚨🚨");
+            }else if (time > 2000) {
+                logger.warn("⚠️⚠️⚠️⚠️⚠️ 해당 메서드의 실행 시간이 2000ms를 초과했습니다. ⚠️⚠️⚠️⚠️⚠️");
+            }else if (time > 1000) {
+                logger.warn("🤫🤫🤫🤫🤫 해당 메서드의 실행 시간이 1000ms를 초과했습니다. 🤫🤫🤫🤫🤫");
             }
         }
     }
