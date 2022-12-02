26e4d0d212ef33171aca68a64f589ac8da32fc23
Tldkt <spooning.Tldkt@gmail.com>
2022-12-02 19:53:45 +0900

    test : querydsl 테스트

--- server/morak_back_end/src/test/java/com/morakmorak/morak_back_end/repository/answer_repository/AnswerQueryRepositoryTest.java
+++ server/morak_back_end/src/test/java/com/morakmorak/morak_back_end/repository/answer_repository/AnswerQueryRepositoryTest.java
@@ -4,9 +4,11 @@ import com.morakmorak.morak_back_end.config.JpaQueryFactoryConfig;
 import com.morakmorak.morak_back_end.entity.Answer;
 import com.morakmorak.morak_back_end.entity.Article;
 import com.morakmorak.morak_back_end.entity.User;
+import com.morakmorak.morak_back_end.entity.enums.ArticleStatus;
 import com.morakmorak.morak_back_end.entity.enums.Grade;
 import com.morakmorak.morak_back_end.repository.answer.AnswerQueryRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static com.morakmorak.morak_back_end.util.TestConstants.NICKNAME1;
 import static com.morakmorak.morak_back_end.util.TestConstants.NICKNAME2;
@@ -98,6 +101,61 @@ public class AnswerQueryRepositoryTest {
 
         assertThat(answer_1.getIsPicked()).isEqualTo(true);
         assertThat(answer2_time).isAfter(answer3_time);
+    }
+    @Test
+    @DisplayName("해당 게시글에 해당되는 답변만 필터링 확인")
+    void getAnswersPickedFirst_count() {
+        //given 해당 게시글에 해당되는 답변만 필터링된
+        Article user_article_2 = Article.builder().user(user).articleStatus(ArticleStatus.POSTING).build();
+        Answer other_answer_2 = Answer.builder().user(other).article(user_article_2).build();
+        user_article_2.getAnswers().add(other_answer_2);
+
+        PageRequest pageRequest = PageRequest.of(0, 5);
+        Long articleId = user_article.getId();
+        //when
+        Page<Answer> target = answerQueryRepository.findAllByArticleId_PickedFirst(articleId, pageRequest);
+
+        long total_count = target.getTotalElements();
+        Answer answer_1 = target.getContent().get(0);
+        Answer answer_2 = target.getContent().get(1);
+        Answer answer_3 = target.getContent().get(2);
+
+        //then
+        assertThat(total_count).isEqualTo(3L);
+        assertThat(answer_1.getArticle().getId()).isEqualTo(user_article.getId());
+    }
+    @Test
+    @DisplayName("삭제된 글의 답변이 유저에게 조회되지 않음 확인")
+    void findAnswersByUserId_posting_only() {
+        //given
+        Article user_article_deleted = Article.builder().user(user).articleStatus(ArticleStatus.REMOVED).build();
+        Answer other_answer_2 = Answer.builder().user(other).article(user_article_deleted).build();
+        user_article_deleted.getAnswers().add(other_answer_2);
+
+        Long userId = other.getId();
+        PageRequest pageRequest = PageRequest.of(0, 50);
+
+        //when,then
+        Page<Answer> target = answerQueryRepository.findAnswersByUserId(userId, pageRequest);
+        List<Answer> other_answer_list = target.getContent();
+        ArticleStatus article_status =other_answer_list.get(0).getArticle().getArticleStatus();
+
+        Assertions.assertThat(other_answer_list.size()).isEqualTo(1);
+        Assertions.assertThat(article_status).isEqualTo(ArticleStatus.POSTING);
+    }
+    @Test
+    @DisplayName("유저 활동내역이 없어도 에러 발생하지 않음 확인")
+    void findAnswersByUserId() {
+        //given
+        PageRequest pageRequest = PageRequest.of(0, 50);
+        Long userId = user.getId();
+        user_article.changeArticleStatus(ArticleStatus.REMOVED);
+
+        //when
+        Page<Answer> target = answerQueryRepository.findAnswersByUserId(userId, pageRequest);
+        List<Answer> user_answer_list = target.getContent();
+
+        Assertions.assertThat(user_answer_list.size()).isEqualTo(0);
     }
 
 }
