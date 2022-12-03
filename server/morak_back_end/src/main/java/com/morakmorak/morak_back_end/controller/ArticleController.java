440d9d5af52511c43ce1fd754a08ae47735251d0
시온백 <tldhs05222@naver.com>
2022-12-03 20:18:41 +0900

    Feat : 게시글 등록 컨트롤러 로직 수정

--- server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/controller/ArticleController.java
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/controller/ArticleController.java
@@ -32,17 +32,12 @@ public class ArticleController {
     private final FileMapper fileMapper;
     private final CategoryMapper categoryMapper;
 
-
     @PostMapping
     public ResponseEntity uploadArticle(@Valid @RequestBody ArticleDto.RequestUploadArticle requestUploadArticle,
                                         @RequestUser UserDto.UserInfo userInfo) {
-        log.info("UPLOAD_ARTICLE_INPUT_{}", requestUploadArticle);
+
         Article article = articleMapper.requestUploadArticleToEntity(requestUploadArticle);
-        List<TagDto.SimpleTag> tags = tagMapper.requestTagWithIdAndNameToTagDto(requestUploadArticle);
-        List<FileDto.RequestFileWithId> files = fileMapper.RequestFileWithIdToFile(requestUploadArticle);
-        Category category = categoryMapper.RequestUploadArticleToCategory(requestUploadArticle);
-        ArticleDto.ResponseSimpleArticle upload = articleService.upload(article, userInfo, tags, files, category);
-        log.info("UPLOAD_ARTICLE_OUTPUT_{}", upload);
+        ArticleDto.ResponseSimpleArticle upload = articleService.upload(article,userInfo);
         return new ResponseEntity<>(upload, HttpStatus.CREATED);
     }
 
@@ -50,23 +45,16 @@ public class ArticleController {
     public ResponseEntity updateArticle(@Valid @RequestBody ArticleDto.RequestUpdateArticle requestUpdateArticle,
                                         @RequestUser UserDto.UserInfo userInfo,
                                         @PathVariable("article-id") Long articleId) {
-        log.info("UPDATE_ARTICLE_INPUT_{}", requestUpdateArticle);
-        log.info("UPDATE_ARTICLE_USERINFO_INPUT_{}", userInfo);
-
         Article article = articleMapper.requestUpdateArticleToEntity(requestUpdateArticle, articleId);
         List<TagDto.SimpleTag> tags = tagMapper.requestTagWithIdAndNameToTagDto(requestUpdateArticle);
         List<FileDto.RequestFileWithId> files = fileMapper.RequestFileWithIdToFile(requestUpdateArticle);
         ArticleDto.ResponseSimpleArticle update = articleService.update(article, userInfo, tags, files);
-
-        log.info("UPDATE_ARTICLE_OUTPUT_{}", update);
         return new ResponseEntity<>(update, HttpStatus.OK);
     }
 
     @DeleteMapping("/{article-id}")
     public ResponseEntity deleteArticle(@PathVariable("article-id") Long articleId,
                                         @RequestUser UserDto.UserInfo userInfo) {
-        log.info("DELETE_ARTICLE_INPUT_{}", articleId);
-        log.info("DELETE_ARTICLE_USERINFO_INPUT_{}", userInfo);
         articleService.deleteArticle(articleId, userInfo);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
@@ -78,36 +66,24 @@ public class ArticleController {
                                         @Param("sort") String sort,
                                         @RequestParam("page") Integer page,
                                         @RequestParam("size") Integer size) {
-        log.info("SEARCHARTICLE_INPUT_PARAM_{}",category);
-        log.info("SEARCHARTICLE_INPUT_PARAM_{}",keyword);
-        log.info("SEARCHARTICLE_INPUT_PARAM_{}",target);
-        log.info("SEARCHARTICLE_INPUT_PARAM_{}",sort);
-        log.info("SEARCHARTICLE_INPUT_PARAM_{}",page);
-        log.info("SEARCHARTICLE_INPUT_PARAM_{}",size);
-
-        ResponseMultiplePaging responseMultiplePaging = articleService.searchArticleAsPaging(category, keyword, target, sort, page, size);
-
-        log.info("SEARCHARTICLE_OUTPUT_{}",responseMultiplePaging);
+        ResponseMultiplePaging responseMultiplePaging =
+                articleService.searchArticleAsPaging(category, keyword, target, sort, page, size);
         return new ResponseEntity(responseMultiplePaging, HttpStatus.OK);
     }
 
     @GetMapping("/{article-id}")
     public ResponseEntity findDetailArticle(@RequestUser UserDto.UserInfo userInfo,
-                                          @PathVariable("article-id") Long articleId) {
-        log.info("FIND_DETAIL_ARTICLE_INPUT_{}", articleId);
-        log.info("FIND_DETAIL_ARTICLE_USERINFO_INPUT_{}", userInfo);
-        ArticleDto.ResponseDetailArticle result = articleService.findDetailArticle(articleId, userInfo);
-        log.info("FIND_DETAIL_ARTICLE_OUTPUT_{}", result);
+                                            @PathVariable("article-id") Long articleId) {
+        ArticleDto.ResponseDetailArticle result =
+                articleService.findDetailArticle(articleId, userInfo);
         return new ResponseEntity(result, HttpStatus.OK);
     }
 
     @PostMapping("/{article-id}/likes")
     public ResponseEntity pressLikeButton(@RequestUser UserDto.UserInfo userInfo,
                                           @PathVariable("article-id") Long articleId) {
-        log.info("PRESS_LIKE_BUTTON_USERINFO_INPUT_{}",userInfo);
-        log.info("PRESS_LIKE_BUTTON_INPUT_{}",articleId);
-        ArticleDto.ResponseArticleLike responseArticleLike = articleService.pressLikeButton(articleId, userInfo);
-        log.info("PRESS_LIKE_BUTTON_OUTPUT_{}", responseArticleLike);
+        ArticleDto.ResponseArticleLike responseArticleLike =
+                articleService.pressLikeButton(articleId, userInfo);
         return new ResponseEntity(responseArticleLike, HttpStatus.OK);
     }
 
@@ -115,9 +91,9 @@ public class ArticleController {
     public ResponseEntity reportArticle(@PathVariable("article-id") Long articleId,
                                         @RequestBody ArticleDto.RequestReportArticle reportArticle,
                                         @RequestUser UserDto.UserInfo userInfo) {
+        Report report = articleMapper.requestReportArticleToReport(reportArticle);
         ArticleDto.ResponseReportArticle responseReportArticle =
-                articleService.reportArticle(articleId, userInfo, articleMapper.requestReportArticleToReport(reportArticle));
-
+                articleService.reportArticle(articleId, userInfo, report);
         return new ResponseEntity(responseReportArticle, HttpStatus.CREATED);
     }
 
