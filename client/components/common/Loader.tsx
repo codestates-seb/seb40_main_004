558755d37d722370d9c00c4d7bdf19c88479c01b
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-12-05 12:24:23 +0900

    chore : 필요없는 부분 삭제

--- client/components/common/Loader.tsx
+++ client/components/common/Loader.tsx
@@ -2,9 +2,5 @@ import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Loader = () => {
-  return (
-    <article>
-      <FontAwesomeIcon className="text-2xl" icon={faCircleNotch} />
-    </article>
-  );
+  return <FontAwesomeIcon className="text-2xl" icon={faCircleNotch} />;
 };
