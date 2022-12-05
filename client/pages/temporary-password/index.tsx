75245b50c7c804dbc1df9e214650c064f33d44c3
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-12-05 19:19:18 +0900

    feat : 임시 비밀번호 발급 후 로그인페이지로 이동하도록 설정

--- client/pages/temporary-password/index.tsx
+++ client/pages/temporary-password/index.tsx
@@ -2,8 +2,10 @@ import axios from 'axios';
 import { useRouter } from 'next/router';
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 import { userEmailAtom } from '../../atomsHS';
+import { Footer } from '../../components/common/Footer';
+import { Header } from '../../components/common/Header';
 
 type TemporaryPasswordProps = {
   email: string;
@@ -14,45 +16,47 @@ const TemporaryPassword = () => {
   const { register, handleSubmit } = useForm<TemporaryPasswordProps>({
     mode: 'onChange',
   });
-  const [email, setEmail] = useRecoilState(userEmailAtom);
+  const setEmail = useSetRecoilState(userEmailAtom);
   const router = useRouter();
   const onValid = ({ email, authKey }: TemporaryPasswordProps) => {
     axios
       .post(`/api/auth/password/recovery`, { email, authKey })
-      .then((res) => {
+      .then(() => {
         setEmail(email);
-        console.log('res2', res);
         alert('임시 비밀번호가 발급되었습니다! 메일을 확인해주세요.');
+        router.push('/login');
       })
       .catch((error) => console.error('error', error));
-
-    // router.push('/signup');
   };
   return (
-    <form onSubmit={handleSubmit(onValid)} className="w-full">
-      <label className="font-bold flex-col flex mx-2">이메일</label>
-      <input
-        {...register('email', { required: true })}
-        className="rounded-full w-96 h-10 
-        pl-4
-        border
-        mb-5
-        mx-2
-        "
-        type="text"
-        placeholder="이메일을 입력해주세요."
-      />
-      <label className="font-bold flex-col flex mx-2">인증번호</label>
-      <input
-        {...register('authKey', { required: true })}
-        type="text"
-        placeholder="인증번호를 입력하세요."
-        className="rounded-full w-full h-10 pl-4 border my-5 mx-2"
-      />
-      <button className="bg-main-yellow py-3 w-full rounded-[20px] font-bold mb-5">
-        인증하기
-      </button>
-    </form>
+    <>
+      <Header />
+      <main className="flex flex-col justify-center items-center h-[79vh] bg-white">
+        <form
+          onSubmit={handleSubmit(onValid)}
+          className="flex flex-col mx-auto justify-center items-start mt-10 space-y-2"
+        >
+          <label className="font-bold flex-col flex mx-2">이메일</label>
+          <input
+            {...register('email', { required: true })}
+            className="rounded-full w-96 h-10 pl-4 border"
+            type="text"
+            placeholder="이메일을 입력해주세요."
+          />
+          <label className="font-bold flex-col flex mx-2">인증번호</label>
+          <input
+            {...register('authKey', { required: true })}
+            type="text"
+            placeholder="인증번호를 입력하세요."
+            className="rounded-full w-96 h-10 pl-4 border"
+          />
+          <button className="bg-main-yellow bg-opacity-80 py-3 w-full rounded-[20px] font-bold mb-5 hover:bg-main-yellow">
+            임시 비밀번호 발급
+          </button>
+        </form>
+      </main>
+      <Footer />
+    </>
   );
 };
 
