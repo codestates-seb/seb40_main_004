95a8d5d32789de273f2ae51f6c4fa6be3a174a7a
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-12-05 19:17:47 +0900

    style : Footer,Header 추가

--- client/pages/find-password-email/index.tsx
+++ client/pages/find-password-email/index.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
+import { Footer } from '../../components/common/Footer';
+import { Header } from '../../components/common/Header';
 
 type FindPasswordProps = {
   email: string;
@@ -14,25 +16,30 @@ const findPassWordWithEmail = () => {
   const onValid = ({ email }: FindPasswordProps) => {
     axios
       .post(`/api/auth/password/support`, { email })
-      .then((res) => {
-        console.log('res1', res);
+      .then(() => {
         router.push('/temporary-password');
       })
-
       .catch((error) => console.error('error', error));
   };
   return (
-    <form onSubmit={handleSubmit(onValid)}>
-      <input
-        {...register('email', { required: true })}
-        type="text"
-        placeholder="이메일을 입력하세요."
-        className="rounded-full w-full h-10 pl-4 border my-5"
-      />
-      <button className="bg-main-yellow py-3 flex justify-center mx-auto w-1/2 rounded-[20px] font-bold mb-5">
-        인증번호 발송
-      </button>
-    </form>
+    <>
+      <Header />
+      <main className="flex flex-col justify-center items-center h-[79vh] bg-white">
+        <form onSubmit={handleSubmit(onValid)} className="space-y-2">
+          <label className="font-bold flex-col flex mx-2 mb-2">이메일</label>
+          <input
+            {...register('email', { required: true })}
+            type="text"
+            placeholder="이메일을 입력하세요."
+            className="rounded-full w-full h-10 pl-4 border"
+          />
+          <button className="bg-main-yellow bg-opacity-80 py-3 w-full rounded-[20px] font-bold  hover:bg-main-yellow">
+            인증번호 발송
+          </button>
+        </form>
+      </main>
+      <Footer />
+    </>
   );
 };
 
