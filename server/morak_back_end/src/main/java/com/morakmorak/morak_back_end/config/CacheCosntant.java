10fedac5f1f8a5572760058b1fa3b4f7f61dbc7d
yangddoddi <pud1251@gmail.com>
2022-12-07 05:52:45 +0900

    feat : 데이터 캐싱을 위한 RedisCacheManager 설정

---
+++ server/morak_back_end/src/main/java/com/morakmorak/morak_back_end/config/CacheCosntant.java
@@ -0,0 +1,7 @@
+package com.morakmorak.morak_back_end.constant;
+
+public class CacheCosntant {
+    public static final String USER_RANK = "userRankList";
+    public static final String MAIN_ARTICLE_LIST = "mainArticleList";
+    public static final String JOB_CALENDAR = "jobCalendar";
+}
