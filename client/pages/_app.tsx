a605a6ffbb9e93e86703d3887d25a159ea57e469
Hyejung Park <hyejj19@naver.com>
2022-12-06 17:07:46 +0900

    chore : fontAwesome 적용시 SSR 사전 렌더링 방지 코드 추가

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -4,6 +4,10 @@ import '../styles/globals.css';
 import 'react-quill/dist/quill.snow.css';
 import '@fullcalendar/common/main.css';
 import '@fullcalendar/daygrid/main.css';
+// fontAwesome 미리 렌더링 방지
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
