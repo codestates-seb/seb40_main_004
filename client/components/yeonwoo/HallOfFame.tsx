41d45db129bf050b08a161d48bf451f86e2cfec4
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-12-13 20:16:12 +0900

    feat : 웹 접근성 추가 - Image 컴포넌트에 alt attribute 적용

--- client/components/yeonwoo/HallOfFame.tsx
+++ client/components/yeonwoo/HallOfFame.tsx
@@ -42,7 +42,12 @@ export const HallOfFame = () => {
                 <Link href={`/dashboard/${rank.userId}`}>
                   <div className="w-[45px] h-[45px] rounded-full overflow-hidden hover:cursor-pointer">
                     {errorAvatarId.includes(rank.avatar?.avatarId) ? (
-                      <Image src="/favicon.ico" width="45px" height="45px" />
+                      <Image
+                        src="/favicon.ico"
+                        width="45px"
+                        height="45px"
+                        alt="rank"
+                      />
                     ) : (
                       <Image
                         src={
@@ -50,6 +55,7 @@ export const HallOfFame = () => {
                         }
                         width="45px"
                         height="45px"
+                        alt="rank"
                         onError={() =>
                           setIsErrorAvatarId((prev) => [
                             ...prev,
