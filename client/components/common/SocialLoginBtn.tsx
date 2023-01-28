248d154d726a746b29ffa25cec9f8601b5898752
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-01-28 21:25:03 +0900

    fix : 카카오 리다이렉트 uri 수정

--- client/components/common/SocialLoginBtn.tsx
+++ client/components/common/SocialLoginBtn.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 export const SocialLoginBtn = () => {
   return (
     <section className="flex w-full justify-center">
-      <Link href="http://morak-873888559.ap-northeast-2.elb.amazonaws.com:8080/oauth2/authorization/kakao">
+      <Link href="https://morak.link/oauth2/authorization/kakao">
         <button className="bg-gray-200 hover:bg-gray-300 cursor-pointer w-full p-3 rounded-[20px]">
           <FontAwesomeIcon
             icon={faComment}
