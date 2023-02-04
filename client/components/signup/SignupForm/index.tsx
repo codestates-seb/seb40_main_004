a57dcb8e5927ac5b1385847e7d8ce6ec262d6ded
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-02-04 20:35:00 +0900

    feat : 회원가입 필드 값 실시간 감지 - 필드 값 입력하는 부분을 감지하여 오류가 있을 경우 바로 메시지 보여주기 - errors optional 처리

--- client/components/signup/SignupForm/index.tsx
+++ client/components/signup/SignupForm/index.tsx
@@ -77,15 +77,13 @@ export const SignUpForm = () => {
             register={{
               ...register('nickname', {
                 required: '닉네임을 입력해주세요.',
-                pattern: {
-                  value: /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣].{0,6}$/,
-                  message: '자음/모음 1자리 이상, 7자리 이하여야 합니다.',
-                },
+                pattern: /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣].{0,6}$/,
               }),
             }}
-            errors={errors.nickname?.message}
           />
-
+          {errors.nickname?.type === 'pattern' ? (
+            <p>자음/모음 1자리 이상, 7자리 이하여야 합니다.</p>
+          ) : null}
           <Input
             label="이메일"
             type="email"
@@ -93,15 +91,13 @@ export const SignUpForm = () => {
             register={{
               ...register('email', {
                 required: '이메일을 입력해주세요.',
-                pattern: {
-                  value: /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i,
-                  message: '이메일이 형식에 맞지 않습니다.',
-                },
+                pattern: /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i,
               }),
             }}
-            errors={errors.email?.message}
           />
-
+          {errors.email?.type === 'pattern' ? (
+            <p>이메일이 형식에 맞지 않습니다.</p>
+          ) : null}
           <Input
             label="비밀번호"
             type="password"
@@ -109,17 +105,16 @@ export const SignUpForm = () => {
             register={{
               ...register('password', {
                 required: '비밀번호를 입력해주세요.',
-                pattern: {
-                  value:
-                    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\d~!@#$%^&*()+|=]{8,16}$/i,
-                  message:
-                    '비밀번호는 8~16자, 영어 대소문자,특수문자가 포함되어야 합니다.',
-                },
+                pattern:
+                  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\d~!@#$%^&*()+|=]{8,16}$/i,
               }),
             }}
-            errors={errors.password?.message}
           />
-
+          {errors.password?.type === 'pattern' ? (
+            <p>
+              비밀번호는 8~16자, 영어 대소문자,특수문자가 포함되어야 합니다.
+            </p>
+          ) : null}
           <Input
             label="비밀번호 확인"
             type="password"
@@ -129,7 +124,6 @@ export const SignUpForm = () => {
                 required: '비밀번호를 한번 더 입력해주세요.',
               }),
             }}
-            errors={errors.confirmPassword?.message}
           />
         </div>
 
