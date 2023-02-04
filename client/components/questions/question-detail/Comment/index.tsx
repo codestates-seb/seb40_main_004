36dad8632aa331050eb3d9b16ee251fef705c8e1
Hyejung Park <hyejj19@naver.com>
2023-02-04 19:57:27 +0900

    ♻️ Refactor :  파일명 변경 및 toastify 적용

---
+++ client/components/questions/question-detail/Comment/index.tsx
@@ -0,0 +1,108 @@
+import { useRouter } from 'next/router';
+import { Comment } from './Comment';
+import { CommentTextArea } from './CommentTextArea';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+
+import { CommentResp } from '@type/comment';
+import { useFetch } from '@libs/useFetchSWR';
+import { useState } from 'react';
+
+type CommentList = {
+  commentPreview?: CommentResp;
+  answerId?: number;
+  url: string;
+  comments: CommentResp[];
+};
+
+const CommentList = ({
+  comments,
+  commentPreview,
+  url,
+  answerId,
+}: CommentList) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return isOpen ? (
+    <>
+      {comments.map((comment: CommentResp) => (
+        <Comment
+          key={comment.commentId}
+          commentId={comment.commentId}
+          articleId={comment.articleId}
+          content={comment.content}
+          createdAt={comment.createdAt}
+          lastModifiedAt={comment.lastModifiedAt}
+          userInfo={comment.userInfo}
+          avatar={comment.avatar}
+          answerId={comment.answerId}
+        />
+      ))}
+      <button className="text-base" onClick={() => setIsOpen(false)}>
+        {`닫기 `}
+        <FontAwesomeIcon icon={faChevronUp} />
+      </button>
+    </>
+  ) : (
+    <>
+      <Comment
+        key={comments[0].commentId}
+        commentId={comments[0].commentId}
+        articleId={comments[0].articleId}
+        content={comments[0].content}
+        createdAt={comments[0].createdAt}
+        lastModifiedAt={comments[0].lastModifiedAt}
+        userInfo={comments[0].userInfo}
+        avatar={comments[0].avatar}
+        answerId={comments[0].answerId}
+      />
+      {comments.length > 1 && (
+        <button className="text-base" onClick={() => setIsOpen(true)}>
+          {`펼치기 `}
+          <FontAwesomeIcon icon={faChevronDown} />
+        </button>
+      )}
+    </>
+  );
+};
+
+type CommentContainerProps = {
+  answerId?: number;
+  comments?: CommentResp;
+  commentPreview?: CommentResp;
+};
+
+export const CommentContainer = ({
+  answerId,
+  commentPreview,
+}: CommentContainerProps) => {
+  // 현재 게시글 id
+  const router = useRouter();
+  const { articleId } = router.query;
+
+  // 답변 코멘트 || 질문 코멘트에 따른 요청 url
+  const url = answerId
+    ? `/api/answers/${answerId}/comments`
+    : `/api/articles/${articleId}/comments`;
+
+  // 코멘트 조회 요청 로직
+  const { data: comments } = useFetch(url) || [];
+
+  return (
+    <>
+      <h3 className="text-xl font-bold">{comments?.length || 0} 코멘트</h3>
+      <section className="space-y-8 pt-4">
+        {comments?.length > 0 && (
+          <CommentList
+            commentPreview={commentPreview}
+            url={url}
+            answerId={answerId}
+            comments={comments}
+          />
+        )}
+        <CommentTextArea answerId={answerId} />
+      </section>
+    </>
+  );
+};
