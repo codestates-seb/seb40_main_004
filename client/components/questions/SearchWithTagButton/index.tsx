53e657dde1e526daa4c80415c47184b9ffcebf05
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-02-17 17:04:08 +0900

    fix : getTagList 함수 구현 로직 다시 변경

--- client/components/questions/SearchWithTagButton/index.tsx
+++ client/components/questions/SearchWithTagButton/index.tsx
@@ -30,18 +30,10 @@ type KeyProps = {
 };
 
 const getTagList = () => {
-  const tagList: { tagId: number; name: string }[][] = [[]];
-  let currentList: { tagId: number; name: string }[] = [];
-
-  tags.forEach((tag, index) => {
-    currentList.push(tag);
-    // 3개 태그가 들어갔을 때 tagList에 currentList 집어넣고
-    if ((index + 1) % 3 === 0) {
-      tagList.push(currentList);
-      currentList = []; // currentList는 빈 배열로 초기화
-    }
-  });
-  if (currentList.length > 0) tagList.push(currentList);
+  const tagList = [];
+  for (let i = 0; i < tags.length; i += 3) {
+    tagList.push(tags.slice(i, i + 3));
+  }
   return tagList;
 };
 
