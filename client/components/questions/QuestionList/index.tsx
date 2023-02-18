03af27c2c7a0a0a73263f65a405d77f071b8cfce
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-02-18 13:57:53 +0900

    style : 상세 페이지 이동 방법 변경 - 글 제목 뿐만 아니라 각 질문 리스트 컴포넌트를 클릭해도 상세 페이지로 넘어가도록 변경

--- client/components/questions/QuestionList/index.tsx
+++ client/components/questions/QuestionList/index.tsx
@@ -26,60 +26,60 @@ export const QuestionList = ({
     return (
       <main className="flex flex-col w-full divide-y min-h-screen">
         {response.data.map((article: ArticleListProps) => (
-          <section
-            className="py-4 space-y-4 hover:bg-gray-100 cursor-pointer"
-            key={article.articleId}
-          >
-            <article className="space-x-2 ">
-              {article.isClosed ? (
-                <FontAwesomeIcon
-                  icon={solidCheck}
-                  className="fa-lg text-main-orange"
-                />
-              ) : (
-                <FontAwesomeIcon icon={voidCheck} className="fa-lg" />
-              )}
+          <Link href={`/questions/${article.articleId}`}>
+            <section
+              className="py-4 space-y-4 hover:cursor-pointer hover:bg-gray-100"
+              key={article.articleId}
+            >
+              <article className="space-x-2">
+                {article.isClosed ? (
+                  <FontAwesomeIcon
+                    icon={solidCheck}
+                    className="fa-lg text-main-orange"
+                  />
+                ) : (
+                  <FontAwesomeIcon icon={voidCheck} className="fa-lg" />
+                )}
 
-              <Link href={`/questions/${article.articleId}`}>
                 <span className="text-lg font-bold hover:cursor-pointer">
                   {article?.title?.length >= 35
                     ? `${article?.title?.slice(0, 35)}...`
                     : article?.title}
                 </span>
-              </Link>
-            </article>
-            <section className="flex justify-between items-center">
-              <article className="flex space-x-3">
-                <div className="flex">
-                  <Link href={`/dashboard/${article?.userInfo?.userId}`}>
-                    <span className="text-xs hover:cursor-pointer">
-                      {article?.userInfo?.nickname}
+              </article>
+              <section className="flex justify-between items-center">
+                <article className="flex space-x-3">
+                  <div className="flex">
+                    <Link href={`/dashboard/${article?.userInfo?.userId}`}>
+                      <span className="text-xs hover:cursor-pointer">
+                        {article?.userInfo?.nickname}
+                      </span>
+                    </Link>
+                  </div>
+                  <div className="text-xs space-x-2">
+                    {article?.tags?.map((tag, i) => (
+                      <span key={i}>{i < 3 ? `#${tag.name}` : ''}</span>
+                    ))}
+                  </div>
+                </article>
+                <article className="flex gap-4">
+                  <div className="flex">
+                    <span className="text-xs">
+                      {elapsedTime(article.createdAt)}
                     </span>
-                  </Link>
-                </div>
-                <div className="text-xs space-x-2">
-                  {article?.tags?.map((tag, i) => (
-                    <span key={i}>{i < 3 ? `#${tag.name}` : ''}</span>
-                  ))}
-                </div>
-              </article>
-              <article className="flex gap-4">
-                <div className="flex">
-                  <span className="text-xs">
-                    {elapsedTime(article.createdAt)}
-                  </span>
-                </div>
-                <div className="flex gap-2">
-                  <FontAwesomeIcon icon={faHeart} size="xs" />
-                  <span className="text-xs">{article.likes}</span>
-                </div>
-                <div className="flex gap-2">
-                  <FontAwesomeIcon icon={faComment} size="xs" />
-                  <span className="text-xs">{article.answerCount}</span>
-                </div>
-              </article>
+                  </div>
+                  <div className="flex gap-2">
+                    <FontAwesomeIcon icon={faHeart} size="xs" />
+                    <span className="text-xs">{article.likes}</span>
+                  </div>
+                  <div className="flex gap-2">
+                    <FontAwesomeIcon icon={faComment} size="xs" />
+                    <span className="text-xs">{article.answerCount}</span>
+                  </div>
+                </article>
+              </section>
             </section>
-          </section>
+          </Link>
         ))}
         <div className="mx-auto mt-10">
           <Pagination
