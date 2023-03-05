136a63bac5d8a624e09f9bba91ff5cae2541cff4
시온백 <tldhs05222@naver.com>
2023-03-06 05:59:02 +0900

    test : Integration.answer패키지 내부 클래스 / @EnabledIfEnvironmentVariable(named = "REDIS", matches = "redis") 어노테이션 제거

--- server/morak_back_end/src/test/java/com/morakmorak/morak_back_end/Integration/answer/UpdateAnswerTest.java
+++ server/morak_back_end/src/test/java/com/morakmorak/morak_back_end/Integration/answer/UpdateAnswerTest.java
@@ -15,7 +15,6 @@ import com.morakmorak.morak_back_end.security.util.JwtTokenUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -42,7 +41,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         "jwt.refreshKey=only_test_refresh_key_value_gn..rlfdlrkqnwhrgkekspdy"
 })
 @AutoConfigureMockMvc
-@EnabledIfEnvironmentVariable(named = "REDIS", matches = "redis")
 public class UpdateAnswerTest {
     @Autowired
     JwtTokenUtil jwtTokenUtil;
